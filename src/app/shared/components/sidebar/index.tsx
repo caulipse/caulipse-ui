46d1e767edecce6281280171add0970b1e912c59
seonghyeoncho <chosh040@naver.com>
2021-06-01 20:26:16 +0900

    사이드바 수정

--- src/app/shared/components/sidebar/index.tsx
+++ src/app/shared/components/sidebar/index.tsx
@@ -1,30 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import "./index.scss";
 
 const SideBar: React.FC = () => {
-
+  const [currentCategotyId, setCurrentCategoryId] = useState<string>("");
   const history = useHistory();
 
   const onClick = (route: string) => {
     history.push(route);
   }
+  const hhandleDefalutRoute = (route: string, id: string) => {
+    setCurrentCategoryId(id);
+    history.push(route);
+  }
+  interface SidebarSubCategoryType {
+    name: string;
+    route: string;
+  }
+  interface SidebarCategoryType {
+    id: string,
+    name: string;
+    defaltRoute: string;
+    subCategories: SidebarSubCategoryType[];
+  }
+  const sidebarCategories: SidebarCategoryType[] = [
+    {
+      id: "study",
+      name: "스터디",
+      defaltRoute: "/study/list",
+      subCategories: [
+        {
+          name: "스터디",
+          route: "/study/list",
+        },
+        {
+          name: "찾아보기",
+          route: "/study/search",
+        },
+      ]
+    },
+    {
+      id: "post",
+      name: "게시글",
+      defaltRoute: "/post",
+      subCategories: [
+        {
+          name: "게시글",
+          route: "/post",
+        },
+      ]
+    },
+    {
+      id: "nonsub",
+      name: "중앙대 비교과",
+      defaltRoute: "/nonsub",
+      subCategories: []
+    },
+  ];
+
   return (
     <div className="sidebar-con">
-      <div>
-        <button type="button" onClick={() => onClick("/study/list")}>
-          <div>스터디</div>
-        </button>
-        <button type="button" onClick={() => onClick("/study/search")}>
-          <div>찾아보기</div>
-        </button>
+      <div className="sidebar-wrapper">
+        {
+          sidebarCategories.map((category: SidebarCategoryType) => (
+            <div key={category.id} >
+              <button onClick={() => hhandleDefalutRoute(category.defaltRoute, category.id)} type="button">
+                <div>{category.name}</div>
+              </button>
+              { currentCategotyId === category.id ? 
+                category.subCategories.map((sub) => (
+                  <button type="button" key={sub.name} onClick={() => onClick(sub.route)}>
+                    <div>{sub.name}</div>
+                  </button>
+                ))
+                :
+                <></>
+              }
+            </div>
+          ))
+        }
       </div>
-        <button type="button" onClick={() => onClick("/post")}>
-          <div>게시글</div>
-        </button>
-        <button type="button" onClick={() => onClick("/nonsub")}>
-          <div>비교과</div>
-        </button>
     </div>
   );
 };

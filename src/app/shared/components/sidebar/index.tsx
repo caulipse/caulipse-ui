a6ac15ca77258823b8b95f43f7d0d56e22bbffda
seonghyeoncho <chosh040@naver.com>
2021-06-18 23:29:41 +0900

    설정 라우트 추가, 라우트 구조 변경

--- src/app/shared/components/sidebar/index.tsx
+++ src/app/shared/components/sidebar/index.tsx
@@ -1,74 +1,48 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import "./index.scss";
+import {
+  sidebarCategoriesInMain, 
+  sidebarCategoriesInProfile, 
+  SidebarCategoryType 
+} from './sidebarList';
 
 const SideBar: React.FC = () => {
   const [currentCategoryId, setCurrentCategoryId] = useState<string>("");
+  const [List, setList] = useState<SidebarCategoryType[]>(sidebarCategoriesInMain);
+  const locationPath = useLocation().pathname;
   const history = useHistory();
-
-  const onClick = (route: string) => {
-    history.push(route);
-  }
-  const hhandleDefalutRoute = (route: string, id: string) => {
+  
+  const handleDefalutRoute = (route: string, id: string) => {
     setCurrentCategoryId(id);
     history.push(route);
   }
-  interface SidebarSubCategoryType {
-    name: string;
-    route: string;
-  }
-  interface SidebarCategoryType {
-    id: string,
-    name: string;
-    defaltRoute: string;
-    subCategories: SidebarSubCategoryType[];
-  }
-  const sidebarCategories: SidebarCategoryType[] = [
-    {
-      id: "study",
-      name: "스터디",
-      defaltRoute: "/study/list",
-      subCategories: [
-        {
-          name: "스터디",
-          route: "/study/list",
-        },
-        {
-          name: "찾아보기",
-          route: "/study/search",
-        },
-      ]
-    },
-    {
-      id: "post",
-      name: "게시글",
-      defaltRoute: "/post",
-      subCategories: []
-    },
-    {
-      id: "nonsub",
-      name: "중앙대 비교과",
-      defaltRoute: "/nonsub",
-      subCategories: []
-    },
-  ];
+  useEffect(() => {
+    if (locationPath === "/") {
+      setCurrentCategoryId("");
+      setList(sidebarCategoriesInMain);
+    }
+    if (locationPath.includes("profile")) setList(sidebarCategoriesInProfile);
+  }, [locationPath]);
 
   return (
     <div className="sidebar-con">
       <div className="sidebar-wrapper">
         {
-          sidebarCategories.map((category: SidebarCategoryType) => (
-            <div key={category.id} >
-              <button onClick={() => hhandleDefalutRoute(category.defaltRoute, category.id)} type="button">
-                <div>{category.name}</div>
+          List.map((category: SidebarCategoryType) => (
+            <div className="sidebar-bts"key={category.id}>
+              <button onClick={() => handleDefalutRoute(category.defaltRoute, category.id)} type="button">
+                <div className="category-bt">{category.name}</div>
               </button>
-              { currentCategoryId === category.id &&
-                category.subCategories.map((sub) => (
-                  <button type="button" key={sub.name} onClick={() => onClick(sub.route)}>
-                    <div>{sub.name}</div>
-                  </button>
-                ))
-              }
+              <div className="sub-bts">
+                { currentCategoryId === category.id &&
+                  category.subCategories.map((sub) => (
+                    <button type="button" key={sub.name} onClick={() => history.push(sub.route)}>
+                      <div className="sub-bt">{sub.name}</div>
+                    </button>
+                  ))
+                }
+              </div>
             </div>
           ))
         }
---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfilePage = (): JSX.Element => (
+  <div>Profile</div>
+);
+
+export default ProfilePage;

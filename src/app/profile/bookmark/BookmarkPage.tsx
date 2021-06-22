8a5d3ea2644530affb47c4a593d23676d316abf1
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-06-22 23:41:52 +0900

    설정/북마크 emptyComponent 레이아웃
    -대략적인 위치
    -마진, 버튼 스타일링 작업 남아있음

--- src/app/profile/bookmark/BookmarkPage.tsx
+++ src/app/profile/bookmark/BookmarkPage.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './BookmarkPage.scss';
 
 function BookmarkPage() {
     return (
         <div>
-            BookmarkPage
+            <div className='title'>스터디</div>
+            <div className='emptyContainer'>
+                <div className='emptyText'>이런, 아직 북마크된 스터디가 없네요</div>
+                <button className='emptyStudyButton' type="button" onClick={()=>{console.log('클릭')}}>클릭해 보세요!</button>
+            </div>
+            <div className='title'>게시글</div>
+            <div className='emptyContainer'>
+                <div className='emptyText'>이런, 아직 북마크된 게시글이 없네요</div>
+                <button className='emptyBoardButton' type="button" onClick={()=>{console.log('클릭')}}>클릭해 보세요!</button>
+            </div>
         </div>
     )
 }

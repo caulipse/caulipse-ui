2b0f56e32830a49ec45053d280feb83246f01ab5
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-06-23 22:07:07 +0900

    설정 북마크 디자인 변경 및 EmptyBookmarkPage 파일 추가
    -마진 수정
    -클릭 효과

--- src/app/profile/bookmark/BookmarkPage.tsx
+++ src/app/profile/bookmark/BookmarkPage.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import './BookmarkPage.scss';
+import EmptyBookmarkPage from './EmptyBookmarkPage';
 
 function BookmarkPage() {
     return (
-        <div className='container'>
-            <div className='title'>스터디</div>
-            <div className='emptyStudyContainer'>
-                <div className='emptyText'>이런, 아직 북마크된 스터디가 없네요</div>
-                <button className='emptyStudyButton' type="button" onClick={()=>{console.log('클릭')}}><div className='emptyButtonText'>클릭해 보세요!</div></button>
-            </div> 
-            <div className='title'>게시글</div>
-            <div className='emptyBoardContainer'>
-                <div className='emptyText'>이런, 아직 북마크된 게시글이 없네요</div>
-                <button className='emptyBoardButton' type="button" onClick={()=>{console.log('클릭')}}><div className='emptyButtonText'>클릭해 보세요!</div></button>
-            </div>
-            
-        </div>
+        <EmptyBookmarkPage />
     )
 }
 

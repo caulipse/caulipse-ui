d474f70ece17cdd089c212985601e4dcc02f457f
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-07-01 23:10:10 +0900

    설정/북마크 게시물 리스트 flex로 처리

--- src/app/profile/bookmark/BookmarkPage.tsx
+++ src/app/profile/bookmark/BookmarkPage.tsx
@@ -90,11 +90,12 @@ function BookmarkPage() {
                 <ArticleList articles={articles} />
             </div>
             <div className='paginationContainer'>
-                <div>이전</div>
+                <button type="button" className='paginationText'>이전</button>
                 {
-                    Array.from(Array(5).keys()).map(i=><div key={i}>{startPage+i}</div>)
+                    Array.from(Array(5).keys()).map(i=><button key={i} type="button" className='paginationNumber
+                    '>{startPage+i}</button>)
                 }
-                <div>다음</div>
+                <button type="button" className='paginationText'>다음</button>
             </div>
         </div>
     )
@@ -120,11 +121,11 @@ const ArticleList=({
     const HeaderBar=()=>{
         return(
             <div className='articleHeaderContainer'>
-                <div style={{marginLeft:'140px'}} className='articleText'>제목</div>
+                <div className={['articleText', 'articleTitle'].join(' ')}><div style={{marginLeft:'140px'}}>제목</div></div>
                 <div className='articleInfoContainer'>
-                    <div style={{marginRight:'105px'}} className='articleText'>작성자</div>
-                    <div style={{marginRight:'55px'}} className='articleText'>추천수</div>
-                    <div style={{marginRight:'20px'}} className='articleText'>북마크</div>
+                    <div className={['articleText', 'articleInfo'].join(' ')}>작성자</div>
+                    <div className={['articleText', 'articleInfo'].join(' ')}>추천수</div>
+                    <div className={['articleText', 'articleInfo'].join(' ')}>북마크</div>
                 </div>
             </div>
         )
@@ -137,11 +138,11 @@ const ArticleList=({
                 articles.slice(0, 6).map(item=>{
                     return(
                         <div key={item.id} className='articleContainer'>
-                            <div style={{marginLeft:'30px', flexShrink:1,}} className='articleText'>{item.title}</div>
+                            <div className={['articleText', 'articleTitle'].join(' ')}><div style={{marginLeft:'30px'}}>{item.title}</div></div>
                             <div className='articleInfoContainer'>
-                                <div style={{marginRight:'105px'}} className='articleText'>{item.author}</div>
-                                <div style={{marginRight:'55px'}} className='articleText'>{item.recommendation}</div>
-                                <div style={{marginRight:'20px'}} className='articleText'>북마크</div>
+                                <div className={['articleText', 'articleInfo'].join(' ')}>{item.author}</div>
+                                <div className={['articleText', 'articleInfo'].join(' ')}>{item.recommendation}</div>
+                                <div className={['articleText', 'articleInfo'].join(' ')}>북마크</div>
                             </div>
                         </div>
                     )

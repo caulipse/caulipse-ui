c98c13ef0f3bca4bf166541f4ca4a842aa4a7c61
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-07-06 22:51:16 +0900

    설정/북마크 본캐 페이지 UI

--- src/app/profile/profile/my/MyPage.tsx
+++ src/app/profile/profile/my/MyPage.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './MyPage.scss';
+
+import * as Factory from "factory.ts";
 
 function MyPage() {
 
@@ -8,46 +10,156 @@ function MyPage() {
 
     console.log('isMain, ', isMain);
 
-    return (
-        <div className='container'>
-            <div className='title'>프로필 설정</div>
+    interface CategoryInterface{
+        id:number;
+        category:string;
+    }
+
+    const getCategoriesData=(iter:number)=>{
+        const categoryFactory=Factory.Sync.makeFactory<CategoryInterface>({
+            id:Factory.each(i=>i),
+            category:'카테고리',
+        })
+        return categoryFactory.buildList(iter);
+    }
+
+    const [categories, setCategories]=useState<CategoryInterface[]>([]);
+
+    useEffect(()=>{
+        setCategories(getCategoriesData(20));
+    }, [])
+
+    const ProfileImage=()=>{
+        return(
+            <div className='profileImage'/>
+        )
+    }
+
+    const TabBar=()=>{
+        return(
             <div className='rowContainer'>
-                <div className='profileImage'/>
-                <div className={['columnContainer', 'marginContainer'].join(' ')}>
-                    <div className='rowContainer'>
-                        <button 
-                            type="button" 
-                            className={isMain?'mainTabButton':'subTabButton'}
-                            onClick={()=>setIsMain(true)}
-                            
-                        >
-                            본캐
-                        </button>
-                        <button 
-                            type="button" 
-                            onClick={()=>setIsMain(false)}
-                            className={!isMain?'mainTabButton':'subTabButton'}
-                        >
-                            부캐
-                        </button>
-                    </div>
-                    <div style={{marginTop:'30px', marginBottom:'10px'}} className='rowContainer'>
-                        <div className='nicknameText'>닉네임</div>
-                        <div className='nicknameExplainText'>게시판에서 사용하는 닉네임이에요!</div>
-                    </div>
+                <button 
+                    type="button" 
+                    className={isMain?'mainTabButton':'subTabButton'}
+                    onClick={()=>setIsMain(true)}
+                    
+                >
+                    본캐
+                </button>
+                <button 
+                    type="button" 
+                    onClick={()=>setIsMain(false)}
+                    className={!isMain?'mainTabButton':'subTabButton'}
+                >
+                    부캐
+                </button>
+            </div>
+        )
+    }
+
+    const NicknameInput=()=>{
+        return(
+            <div>
+                <div style={{marginTop:'30px', marginBottom:'10px'}} className='rowContainer'>
+                    <div className='titleText'>닉네임</div>
+                    <div className='titleExplainText'>게시판에서 사용하는 닉네임이에요!</div>
+                </div>
+                <div style={{
+                    display:'flex',
+                    width:'100%',
+                }}>
                     <input className='nicknameInput' type='text' placeholder='닉네임을 입력해주세요.'/>
-                    <div style={{marginTop:'40px', marginBottom:'10px'}} className='nicknameText'>학과</div>
-                    <div className='rowContainer'>
+                </div> 
+            </div>
+        )
+    }
+
+    const MajorInput=()=>{
+        return(
+            <div>
+                <div style={{marginTop:'40px', marginBottom:'10px'}} className='nicknameText'>학과</div>
+                <div className='rowContainer'>
+                    <div style={{display:'flex',flex:1,height:'44px', marginRight:'20px'}}>
                         <input type="text" className='nicknameInput'/>
-                        <select name="grade">
+                    </div>
+                    <div style={{display:'flex',flex:1,height:'44px'}}>
+                        <select name="grade" className='gradeSelection'>
                             <option value={1}>1학년</option>
                             <option value={2}>2학년</option>
                             <option value={3}>3학년</option>
                             <option value={4}>4학년</option>
                         </select>
                     </div>
+                    
                 </div>
             </div>
+        )
+    }
+
+    const Introduction=({
+        isShort,
+    }:{
+        isShort:boolean
+    })=>{
+        return(
+            <div className='introductionContainer'>
+                <div className='rowContainer'>
+                    <div className='titleText'>{isShort?'프로필 소개문구':'자기소개 글'}</div>
+                    {isShort||<div className='titleExplainText'>프로필 문구가 너무 짧으신가요? 자기소개글을 완성시켜주세요!!</div>}
+                </div>
+                <textarea placeholder='나만의 소개글을 완성해주세요!' className='introductionTextArea' />
+            </div>
+        )
+    }
+
+    const Categories=()=>{
+        return(
+            <div className='categoryContainer'>
+                <div className='titleText'>관심 카테고리</div>
+                <div className='categoryListContainer'>
+                    {
+                        categories.map(item=>{
+                            return(
+                                <div key={item.id} className='categoryItemContainer'>
+                                    <div className='categoryText'>{item.category}</div>
+                                    <button className='categoryText' type='button'>x</button>
+                                </div>
+                            )
+                        })
+                    }
+                    <div className='addItemContainer'>
+                        <div className='addText'>추가하기</div>
+                        <button className='addText' type='button'>+</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const UpdateButton=()=>{
+        return(
+            <button type="button" className='updateButton'>
+                <div className='updateButtonText'>수정하기</div>
+            </button>
+        )
+    }
+
+    return (
+        <div className='container'>
+            <div className='title'>프로필 설정</div>
+            <div className='rowContainer'>
+                <ProfileImage />
+                <div className={['columnContainer', 'marginContainer'].join(' ')}>
+                    <TabBar />
+                    <NicknameInput />
+                    <MajorInput />
+                    
+                </div>
+            </div>
+            <Introduction isShort/>
+            <Introduction isShort={false} />
+            <Categories />
+            <UpdateButton />
         </div>
     )
 }

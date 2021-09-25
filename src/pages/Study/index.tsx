89833dc22716aa584647c95aa7d9bca12e82bbbc
seonghyeoncho <chosh040@naver.com>
2021-09-25 18:34:06 +0900

    1차 수정 완료

--- src/pages/Study/index.tsx
+++ src/pages/Study/index.tsx
@@ -1,15 +1,37 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import StudyListPage from "@src/app/study/StudyListPage";
 import './styles.scss';
 
-const StudyPage = (): JSX.Element => (
-  <div className="studyPage-con">
-    <div>스터디</div>
-    <Switch>
-      <Route exact path="/study/list" component={StudyListPage}/>
-    </Switch>
-  </div>
-);
+const CategoryObj = {
+  employment: "취업, 면접",
+  certificate: "자격증",
+  programming: "프로그래밍",
+  etc: "생활 기타",
+}
+const StudyPage = (): JSX.Element => {
+  const location = useLocation();
+  
+  const getValueFromCategoryObj = (key: string) => {
+    const path = key.split('/')[2];
+    const result = Object.keys(CategoryObj).indexOf(path);
+    return Object.values(CategoryObj)[result];
+  }
+
+  return (
+    <div className="studyPage-con">
+      <div className="studyPage-title">
+        <span className="studyPage-text">스터디</span>
+        <span className="studyPage-category">&gt; {getValueFromCategoryObj(location.pathname)}</span>
+      </div>
+      <Switch>
+        <Route exact path='/study/employment'component={StudyListPage}/>
+        <Route exact path='/study/certificate'component={StudyListPage}/>
+        <Route exact path='/study/programming'component={StudyListPage}/>
+        <Route exact path='/study/etc'component={StudyListPage}/>
+      </Switch>
+    </div>
+  );
+};
 
 export default StudyPage;

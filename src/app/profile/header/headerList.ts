f2b909a11a44a24347ad30dfb61b5d4e1cbbd8bb
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-12-05 02:52:42 +0900

    feat: profile header 리스트 생성

---
+++ src/app/profile/header/headerList.ts
@@ -0,0 +1,19 @@
+export interface HeaderButtonProps {
+	title: string;
+	route: string;
+}
+
+export const headerButtons: HeaderButtonProps[] = [
+	{
+		title: '북마크',
+		route: '/bookmark',
+	},
+	{
+		title: '신청 스터디',
+		route: '/apply',
+	},
+	{
+		title: '북마크',
+		route: '/bookmark',
+	},
+];

d579db6d91f1f87952f67a9dfdd767f7288ead0b
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-12-07 22:40:45 +0900

    feat: redux logger 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import userReducer from './modules/user';
 
 export const store = configureStore({
 	reducer: {
 		user: userReducer,
 	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;

922f0a59b917891818a158d225e54e3a0df9ffec
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-12-10 23:10:17 +0900

    fix: bookmarkPage 누락된 요소 추가

--- src/pages/Profile/bookmark/BookmarkPage.tsx
+++ src/pages/Profile/bookmark/BookmarkPage.tsx
@@ -1,11 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import * as Factory from 'factory.ts';
-import './BookmarkPage.scss';
 import BookmarkList from '@src/app/profile/bookmark/BookmarkList';
+import EmptyComponent from '@src/app/shared/components/emptyComponents';
+import './BookmarkPage.scss';
 import { BookmarkInterface, StudyInterface } from '../../../app/profile/interface/interface';
 
 const BookmarkPage = (): JSX.Element => {
-	const [recruitingStudies, setRecruitingStudies] = useState<any[]>([]);
+	const [recruitingStudies, setRecruitingStudies] = useState<BookmarkInterface[]>([]);
+	const [recruitedStudiesVisible, setRecruitedStudiesVisible] = useState<boolean>(false);
+
+	const getBookmarkData = (iter: number) => {
+		const bookmarkFactory = Factory.Sync.makeFactory<BookmarkInterface>({
+			studyId: Factory.each((i) => i),
+			title: '제목입니다.',
+			currentNumber: 1,
+			maxNumber: 10,
+			date: new Date(),
+			hits: 5,
+			stars: 5,
+			category: Factory.each((i) => (i > 2 ? '어학->토익' : '프로그래밍')),
+		});
+		return bookmarkFactory.buildList(iter);
+	};
 
 	const RecruitingStudies = () => {
 		const [recruitingShow, setRecruitingShow] = useState<boolean>(true);
@@ -33,7 +49,15 @@ const BookmarkPage = (): JSX.Element => {
 						<span className="deleteAllTextBlurred">모두 삭제</span>
 					</button>
 				</div>
-				{recruitingStudies.length === 0 && recruitingShow && <EmptyRecruitingStudies />}
+				{recruitingStudies.length === 0 && recruitingShow && (
+					<EmptyComponent
+						title="이런, 북마크한 스터디가 없어요."
+						buttonText="스터디 찾아보기"
+						onClick={() => {
+							console.log('스터디 찾아보기 클릭');
+						}}
+					/>
+				)}
 			</>
 		);
 	};

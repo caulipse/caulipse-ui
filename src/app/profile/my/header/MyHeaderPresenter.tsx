3e95d9a4a90e015a34074a132b55a7de4d01501d
Jeyoung-Park <jason9380@naver.com>
2021-12-16 01:29:26 +0900

    feat: 프로필 사진 ui

--- src/app/profile/my/header/MyHeaderPresenter.tsx
+++ src/app/profile/my/header/MyHeaderPresenter.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { IoClose, IoSettingsSharp, IoChevronForwardOutline } from 'react-icons/io5';
 import './index.scss';
 
+const sampleImgUrl='https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__480.jpg';
+
 interface MyHeaderPresenterProps {
 	userName: string;
 }
@@ -17,6 +19,10 @@ const MyHeaderPresenter = ({ userName }: MyHeaderPresenterProps): JSX.Element =>
 					<IoSettingsSharp size={24} fill="#f7f7f7" />
 				</button>
 			</div>
+            <div className='profilePhotoContainer'>
+                <img className='profilePhoto' src={sampleImgUrl} alt='사용자 프로필 사진'/>
+
+            </div>
 			{userName}
 		</div>
 	);

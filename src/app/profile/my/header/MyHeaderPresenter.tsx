9ef33e323da8b6c95ec415461cad436579f510ce
Jeyoung-Park <jason9380@naver.com>
2021-12-16 01:02:50 +0900

    feat: 아이콘 추가

--- src/app/profile/my/header/MyHeaderPresenter.tsx
+++ src/app/profile/my/header/MyHeaderPresenter.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { IoClose, IoSettingsSharp, IoChevronForwardOutline } from 'react-icons/io5';
+import './index.scss';
 
 interface MyHeaderPresenterProps {
 	userName: string;
 }
 
-const MyHeaderPresenter = ({ userName }: MyHeaderPresenterProps):JSX.Element => {
-	return <div>{userName}</div>;
+const MyHeaderPresenter = ({ userName }: MyHeaderPresenterProps): JSX.Element => {
+	return (
+		<div className="container">
+			<div className="iconsContainer">
+				<button type="button">
+					<IoClose size={24} fill="#f7f7f7" />
+				</button>
+				<button type="button">
+					<IoSettingsSharp size={24} fill="#f7f7f7" />
+				</button>
+			</div>
+			{userName}
+		</div>
+	);
 };
 
 export default MyHeaderPresenter;

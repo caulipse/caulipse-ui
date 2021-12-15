31baa7618bf6a637377fa436445c8b18098995fd
Jeyoung-Park <jason9380@naver.com>
2021-12-16 01:45:36 +0900

    feat: x 버튼 클릭 시 뒤로 가기 이벤트 추가

--- src/app/profile/my/header/MyHeaderPresenter.tsx
+++ src/app/profile/my/header/MyHeaderPresenter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IoClose, IoSettingsSharp, IoChevronForwardOutline } from 'react-icons/io5';
+import { useHistory } from 'react-router-dom';
 import './index.scss';
 
 const sampleImgUrl = 'https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__480.jpg';
@@ -9,10 +10,13 @@ interface MyHeaderPresenterProps {
 }
 
 const MyHeaderPresenter = ({ userName }: MyHeaderPresenterProps): JSX.Element => {
+
+    const history=useHistory();
+    
 	return (
 		<div className="container">
 			<div className="iconsContainer">
-				<button type="button">
+				<button type="button" onClick={()=>history.goBack()}>
 					<IoClose size={24} fill="#f7f7f7" />
 				</button>
 				<button type="button">

96255b259075333c245fd4a4f15ae5adfec41e01
Jeyoung-Park <jason9380@naver.com>
2021-12-17 00:15:32 +0900

    feat: preview 컴포넌트 추가

--- src/app/profile/my/preview/PreviewPresenter.tsx
+++ src/app/profile/my/preview/PreviewPresenter.tsx
@@ -7,8 +7,23 @@ interface PreviewPresenterProps {
 	recruitNum: number;
 }
 
-const PreviewPresenter = ({ bookmarkNum, applyNum, recruitNum }: PreviewPresenterProps) => {
-	return <div className='container'>{bookmarkNum}</div>;
+const PreviewPresenter = ({ bookmarkNum, applyNum, recruitNum }: PreviewPresenterProps): JSX.Element => {
+	return (
+		<div className="preview-container">
+			<div className="preview-item">
+				<span className="preview-item-number">{bookmarkNum}</span>
+				<span className="preview-item-title">북마크</span>
+			</div>
+			<div className="preview-item">
+				<span className="preview-item-number">{applyNum}</span>
+				<span className="preview-item-title">신청 중</span>
+			</div>
+			<div className="preview-item">
+				<span className="preview-item-number">{recruitNum}</span>
+				<span className="preview-item-title">모집 중</span>
+			</div>
+		</div>
+	);
 };
 
 export default PreviewPresenter;

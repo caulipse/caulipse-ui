b190e800397af2b1cf7818c12650b85d66d634d5
Jeyoung-Park <jason9380@naver.com>
2021-12-18 23:56:15 +0900

    feat: mypage container 적용

--- src/pages/Profile/my/MyPage.tsx
+++ src/pages/Profile/my/MyPage.tsx
@@ -1,12 +1,13 @@
 import MyHeaderContainer from '@src/app/profile/my/header/MyHeaderContainer';
 import PreviewContainer from '@src/app/profile/my/preview/PreviewContainer';
 import React from 'react';
+import './index.scss';
 
 // interface MyPageProps {}
 
-const MyPage = ():JSX.Element => {
+const MyPage = (): JSX.Element => {
 	return (
-		<div>
+		<div className="my-page-container">
 			<MyHeaderContainer />
 			<PreviewContainer />
 		</div>

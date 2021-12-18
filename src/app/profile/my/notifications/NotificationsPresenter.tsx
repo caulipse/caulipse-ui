4bfe69901b7c3e14bc734b8c3b6a304c831e77ff
Jeyoung-Park <jason9380@naver.com>
2021-12-19 01:02:27 +0900

    feat: 알림 리스트

--- src/app/profile/my/notifications/NotificationsPresenter.tsx
+++ src/app/profile/my/notifications/NotificationsPresenter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BookmarkInterface } from '../../interface/interface';
 import './index.scss';
+import NotiItem from './NotiItem';
 
 interface NotificationsPresenterProps {
 	notifications: BookmarkInterface[];
@@ -10,11 +11,14 @@ const NotificationsPresenter = ({ notifications }: NotificationsPresenterProps):
 	return (
 		<div className="notifications-container">
 			<div className="notifications-header-container">
-				<div className='notifications-header-title'>알림 ({notifications?.length ?? 0})</div>
+				<div className="notifications-header-title">알림 ({notifications?.length ?? 0})</div>
 				<button type="button">
-					<div className='notifications-header-more'>더보기</div>
+					<div className="notifications-header-more">더보기</div>
 				</button>
 			</div>
+			{notifications?.slice(0, 2)?.map((item, index, {length}) => (
+				<NotiItem key={item.studyId} isDivider={index!==length-1} notification={item} />
+			))}
 		</div>
 	);
 };

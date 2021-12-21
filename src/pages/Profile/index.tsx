6479ea76fb34dd84e8cd672381fef94712666816
Jeyoung-Park <jason9380@naver.com>
2021-12-22 00:30:52 +0900

    feat: 프로필 편집 페이지 폴더 및 파일 생성

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import BookmarkPage from './bookmark';
 import MyPage from './my/MyPage';
 import ProfileHeader from '../../app/profile/header';
+import MyProfileEditPage from './myProfileEdit/MyProfileEditPage';
 
 const ProfileContainer = (): JSX.Element => (
 	<Switch>
 		<Route exact path="/profile/:userId" component={MyPage} />
 		<Route exact path="/bookmark" component={BookmarkPage} />
+		<Route exact path="/profile/edit" component={MyProfileEditPage} />
 	</Switch>
 );
 
---
+++ src/pages/Profile/myProfileEdit/MyProfileEditPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyProfileEditPage = () => {
+	return <div>MyProfileEditPage</div>;
+};
+
+export default MyProfileEditPage;

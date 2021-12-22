feec89def0f1296157fae4b72acafb7e1027d4ff
Jeyoung-Park <jason9380@naver.com>
2021-12-23 00:46:48 +0900

    fix: 북마크 페이지에 최대 너비 적용

--- src/pages/Profile/bookmark/index.tsx
+++ src/pages/Profile/bookmark/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
 import BookmarkContainer from '@src/app/profile/bookmark/BookmarkContainer';
+import './index.scss';
 
 const BookmarkPage = (): JSX.Element => {
 	return (
-		<div>
-			<BookmarkContainer />
+		<div className="bookmark-page-bg">
+			<div className="bookmark-page-container">
+				<BookmarkContainer />
+			</div>
 		</div>
 	);
 };

424eb01e78804d9e329400483a45acca3af72f90
Jeyoung-Park <jason9380@naver.com>
2021-12-23 00:23:28 +0900

    feat: 반응형 maxwidth 적용

--- src/pages/Profile/myProfileEdit/MyProfileEditPage.tsx
+++ src/pages/Profile/myProfileEdit/MyProfileEditPage.tsx
@@ -1,10 +1,13 @@
 import MyProfileEditContainer from '@src/app/profile/myProfileEdit/MyProfileEditContainer';
 import React from 'react';
+import './MyProfileEditPage.scss';
 
 const MyProfileEditPage = (): JSX.Element => {
 	return (
-		<div>
-			<MyProfileEditContainer />
+		<div className="my-profile-edit-page-bg">
+			<div className="my-profile-edit-page-container">
+				<MyProfileEditContainer />
+			</div>
 		</div>
 	);
 };

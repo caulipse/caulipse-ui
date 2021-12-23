0cc58aa22d25367c2c7658719ef25a36a503ccf8
Jeyoung-Park <jason9380@naver.com>
2021-12-24 03:20:49 +0900

    feat: 마이페이지 > 북마크, 신청중, 모집중 으로 가는 네비게이션 추가

--- src/app/profile/my/preview/PreviewPresenter.tsx
+++ src/app/profile/my/preview/PreviewPresenter.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './index.scss';
 
 interface PreviewPresenterProps {
@@ -8,20 +9,40 @@ interface PreviewPresenterProps {
 }
 
 const PreviewPresenter = ({ bookmarkNum, applyNum, recruitNum }: PreviewPresenterProps): JSX.Element => {
+	const history = useHistory();
+
+	const navigateToBookmark = () => {
+		history.push('/bookmark');
+	};
+
+	const navigateToAppliedStudies = () => {
+		history.push('/appliedStudies');
+	};
+
+	const navigateToRecruitingStudies = () => {
+		history.push('/recruitingStudies');
+	};
+
 	return (
 		<div className="preview-container">
-			<div className="preview-item">
-				<span className="preview-item-number">{bookmarkNum}</span>
-				<span className="preview-item-title">북마크</span>
-			</div>
-			<div className="preview-item">
-				<span className="preview-item-number">{applyNum}</span>
-				<span className="preview-item-title">신청 중</span>
-			</div>
-			<div className="preview-item">
-				<span className="preview-item-number">{recruitNum}</span>
-				<span className="preview-item-title">모집 중</span>
-			</div>
+			<button type="button" onClick={navigateToBookmark}>
+				<div className="preview-item">
+					<span className="preview-item-number">{bookmarkNum}</span>
+					<span className="preview-item-title">북마크</span>
+				</div>
+			</button>
+			<button type="button" onClick={navigateToAppliedStudies}>
+				<div className="preview-item">
+					<span className="preview-item-number">{applyNum}</span>
+					<span className="preview-item-title">신청 중</span>
+				</div>
+			</button>
+			<button type="button" onClick={navigateToRecruitingStudies}>
+				<div className="preview-item">
+					<span className="preview-item-number">{recruitNum}</span>
+					<span className="preview-item-title">모집 중</span>
+				</div>
+			</button>
 		</div>
 	);
 };

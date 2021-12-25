2beef11bf8721e2bf66c2d65cce69e9af5d1518b
Jeyoung-Park <jason9380@naver.com>
2021-12-25 23:16:24 +0900

    feat: 마감된 항목 디자인

--- src/app/profile/appliedStudies/closedAppliedStudyList/ClosedAppliedStudyItem.tsx
+++ src/app/profile/appliedStudies/closedAppliedStudyList/ClosedAppliedStudyItem.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { IoEllipsisHorizontal, IoEllipsisVertical } from 'react-icons/io5';
+import moment from 'moment';
+import NumberUtils from '@src/app/shared/utils/number';
 import { AppliedStudyInterface } from '../../interface/interface';
 import './index.scss';
 
@@ -7,7 +10,38 @@ interface ClosedAppliedStudyItemProps {
 }
 
 const ClosedAppliedStudyItem = ({ closedAppliedStudyItem }: ClosedAppliedStudyItemProps): JSX.Element => {
-	return <div className="closed-applied-study-item-container"><div>{closedAppliedStudyItem.title}</div><div>참여완료</div></div>;
+	const onClickMenu = () => {
+		console.log('onClickMenu');
+	};
+
+	return (
+		<div className="closed-applied-study-item-container">
+			<div className="closed-applied-study-item-column-container">
+				<div className="closed-applied-study-item-title">{closedAppliedStudyItem.title}</div>
+				<div className="closed-applied-study-item-row-container">
+					<div className="closed-applied-study-item-subtext">
+						{moment(closedAppliedStudyItem.date).format('YY.MM.DD HH:MM')}
+					</div>
+					<div className="closed-applied-study-item-divider-dot">·</div>
+					<div className="closed-applied-study-item-subtext">
+						조회 {NumberUtils.toFormattedCount(closedAppliedStudyItem.hits)}
+					</div>
+					<div className="closed-applied-study-item-divider-dot">·</div>
+					<div className="closed-applied-study-item-subtext">
+						북마크 {NumberUtils.toFormattedCount(closedAppliedStudyItem.bookmarks)}
+					</div>
+				</div>
+			</div>
+			<div className="closed-applied-study-item-row-container">
+				{closedAppliedStudyItem.status === 'accepted' && (
+					<div className="closed-applied-study-item-status">참여완료</div>
+				)}
+				<button type="button" onClick={onClickMenu}>
+					<IoEllipsisHorizontal size={24} color="#b1b1b1" />
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default ClosedAppliedStudyItem;

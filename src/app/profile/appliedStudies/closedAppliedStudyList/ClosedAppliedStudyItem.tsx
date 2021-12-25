2e5cd66a12ae062e68a162197bd6baccc8f5fbc3
Jeyoung-Park <jason9380@naver.com>
2021-12-25 18:00:01 +0900

    feat: 마감된 스터디 container 디자인

--- src/app/profile/appliedStudies/closedAppliedStudyList/ClosedAppliedStudyItem.tsx
+++ src/app/profile/appliedStudies/closedAppliedStudyList/ClosedAppliedStudyItem.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { AppliedStudyInterface } from '../../interface/interface';
+import './index.scss';
 
 interface ClosedAppliedStudyItemProps {
 	closedAppliedStudyItem: AppliedStudyInterface;
 }
 
 const ClosedAppliedStudyItem = ({ closedAppliedStudyItem }: ClosedAppliedStudyItemProps): JSX.Element => {
-	return <div>{closedAppliedStudyItem.title}</div>;
+	return <div className="closed-applied-study-item-container"><div>{closedAppliedStudyItem.title}</div><div>참여완료</div></div>;
 };
 
 export default ClosedAppliedStudyItem;

31343355dcee8a887b5d1e4e52b0d856f9afe99b
Jeyoung-Park <jason9380@naver.com>
2022-01-01 04:50:27 +0900

    feat: 팝업 추가

--- src/app/profile/my/profile/ProfilePopupPresenter.tsx
+++ src/app/profile/my/profile/ProfilePopupPresenter.tsx
@@ -2,6 +2,8 @@ import useWindowDimensions from '@src/hooks/useWindowDimensions';
 import React, { useCallback, useMemo } from 'react';
 import { BottomSheet, BottomSheetProps } from 'react-spring-bottom-sheet';
 import './index.scss';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 interface ProfilePopupPresenterProps {
 	profileSheetVisible: boolean;
@@ -33,36 +35,55 @@ const ProfilePopupPresenter = ({
 	const { width: windowWidth } = useWindowDimensions();
 	console.log('window width', windowWidth);
 
-	return (
-		<BottomSheet open={profileSheetVisible} onDismiss={() => setProfileSheetVisible(false)} scrollLocking>
-			<div className="profile-bottom-sheet-container">
-				<img className="profile-bottom-sheet-profile-img" src={profilePicture} alt="프로필 이미지" />
-				<div className="profile-bottom-sheet-name">{userName}</div>
-				<div className="profile-bottom-sheet-short-about">{shortUserAbout}</div>
-				<div className="profile-bottom-sheet-tag-container">
-					{tags?.map((tagItem, tagIndex) => (
-						<div key={tagItem} className={`profile-bottom-sheet-tag-item ${tagIndex === 0 ? '' : 'ml12'}`}>
-							{tagItem}
-						</div>
-					))}
-				</div>
-				<div className="profile-bottom-sheet-divider" />
-				<div className="profile-bottom-sheet-hashtag-container">
-					<div className="profile-bottom-sheet-hashtag-bold-text">
-						#{dept} <span className="profile-bottom-sheet-hashtag-regular-text">전공</span>&nbsp;
+	const Container = () => {
+		return windowWidth > 1024 ? Popup : BottomSheet;
+	};
+
+	const Content = () => (
+		<div className="profile-bottom-sheet-container">
+			<img className="profile-bottom-sheet-profile-img" src={profilePicture} alt="프로필 이미지" />
+			<div className="profile-bottom-sheet-name">{userName}</div>
+			<div className="profile-bottom-sheet-short-about">{shortUserAbout}</div>
+			<div className="profile-bottom-sheet-tag-container">
+				{tags?.map((tagItem, tagIndex) => (
+					<div key={tagItem} className={`profile-bottom-sheet-tag-item ${tagIndex === 0 ? '' : 'ml12'}`}>
+						{tagItem}
 					</div>
-					<div className="profile-bottom-sheet-hashtag-bold-text">#{grade}학년&nbsp;</div>
-					<div className="profile-bottom-sheet-hashtag-bold-text">#{status}</div>
-				</div>
-				<div className="profile-bottom-sheet-link-container">
-					{links?.map((linkItem) => (
-						<div key={linkItem}>{linkItem}</div>
-					))}
-				</div>
-				<div className="profile-bottom-sheet-divider" />
-				<div className="profile-bottom-sheet-about-title">저는요..</div>
-				<div className="profile-bottom-sheet-about-text">{userAbout}</div>
+				))}
 			</div>
+			<div className="profile-bottom-sheet-divider" />
+			<div className="profile-bottom-sheet-hashtag-container">
+				<div className="profile-bottom-sheet-hashtag-bold-text">
+					#{dept} <span className="profile-bottom-sheet-hashtag-regular-text">전공</span>&nbsp;
+				</div>
+				<div className="profile-bottom-sheet-hashtag-bold-text">#{grade}학년&nbsp;</div>
+				<div className="profile-bottom-sheet-hashtag-bold-text">#{status}</div>
+			</div>
+			<div className="profile-bottom-sheet-link-container">
+				{links?.map((linkItem) => (
+					<div key={linkItem}>{linkItem}</div>
+				))}
+			</div>
+			<div className="profile-bottom-sheet-divider" />
+			<div className="profile-bottom-sheet-about-title">저는요..</div>
+			<div className="profile-bottom-sheet-about-text">{userAbout}</div>
+		</div>
+	);
+
+	if (windowWidth > 1024) {
+		return (
+			<Popup
+				open={profileSheetVisible}
+				onClose={()=>setProfileSheetVisible(false)}
+				position="center center"
+			>
+				<Content />
+			</Popup>
+		);
+	}
+	return (
+		<BottomSheet open={profileSheetVisible} onDismiss={() => setProfileSheetVisible(false)}>
+			<Content />
 		</BottomSheet>
 	);
 };

dd81b9dc8c86ff6c54efdb1e5d1a7f04cf25e40f
Jeyoung-Park <jason9380@naver.com>
2022-01-04 00:22:09 +0900

    fix: BookmarkPage navigation header 수정

--- src/pages/Profile/bookmark/index.tsx
+++ src/pages/Profile/bookmark/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import BookmarkContainer from '@src/app/profile/bookmark/BookmarkContainer';
 import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
+import BookmarkHeader from '../../../app/profile/header';
 
 const BookmarkPage = (): JSX.Element => {
 	return (
-		<div className="bookmark-page-bg">
-			<div className="bookmark-page-container">
-				<BookmarkContainer />
+		<BrowserRouter>
+			<BookmarkHeader />
+			<div className="bookmark-page-bg">
+				<div className="bookmark-page-container">
+					<BookmarkContainer />
+				</div>
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 };
 

47381bf6b4d0ce9d7d3ee9ad4b58fbbbe810b1a2
Jeyoung-Park <jason9380@naver.com>
2022-01-15 01:09:37 +0900

    fix: scroll 시 바텀 시트 올라오도록 구현

--- src/app/profile/my/profile/ProfilePopupPresenter.tsx
+++ src/app/profile/my/profile/ProfilePopupPresenter.tsx
@@ -1,11 +1,12 @@
 import useWindowDimensions from '@src/hooks/useWindowDimensions';
-import React, { useCallback, useMemo } from 'react';
-import { BottomSheet, BottomSheetProps } from 'react-spring-bottom-sheet';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { BottomSheet, BottomSheetProps, BottomSheetRef } from 'react-spring-bottom-sheet';
 import './index.scss';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import ProfileLink from './profileLink/ProfileLink';
 
+const BOTTOMSHEET_MINHEIGHT = 350;
 interface ProfilePopupPresenterProps {
 	profileSheetVisible: boolean;
 	setProfileSheetVisible: (param: boolean) => void;
@@ -33,40 +34,52 @@ const ProfilePopupPresenter = ({
 	userAbout,
 	tags,
 }: ProfilePopupPresenterProps): JSX.Element => {
-	const { width: windowWidth } = useWindowDimensions();
+	const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+	const bottomSheetRef = useRef<BottomSheetRef>(null);
+	const [bottomSheetHeight, setBottomSheetHeight] = useState<number>(334);
 
-	const Content = () => (
-		<div className="profile-bottom-sheet-container">
-			<img className="profile-bottom-sheet-profile-img" src={profilePicture} alt="프로필 이미지" />
-			<div className="profile-bottom-sheet-name">{userName}</div>
-			<div className="profile-bottom-sheet-short-about">{shortUserAbout}</div>
-			<div className="profile-bottom-sheet-tag-container">
-				{tags?.map((tagItem, tagIndex) => (
-					<div key={tagItem} className={`profile-bottom-sheet-tag-item ${tagIndex === 0 ? '' : 'ml12'}`}>
-						{tagItem}
-					</div>
-				))}
-			</div>
-			<div className="profile-bottom-sheet-divider" />
-			<div className="profile-bottom-sheet-hashtag-container">
-				<div className="profile-bottom-sheet-hashtag-bold-text">
-					#{dept} <span className="profile-bottom-sheet-hashtag-regular-text">전공</span>&nbsp;
+	const Content = () => {
+		const handleScroll = (e: React.UIEvent<HTMLElement>) => {
+			if (e.currentTarget.scrollTop > 10) {
+				setBottomSheetHeight(windowHeight - 102);
+			} else {
+				setBottomSheetHeight(BOTTOMSHEET_MINHEIGHT);
+			}
+		};
+
+		return (
+			<div className="profile-bottom-sheet-container" style={{ height: bottomSheetHeight }} onScroll={handleScroll}>
+				<img className="profile-bottom-sheet-profile-img" src={profilePicture} alt="프로필 이미지" />
+				<div className="profile-bottom-sheet-name">{userName}</div>
+				<div className="profile-bottom-sheet-short-about">{shortUserAbout}</div>
+				<div className="profile-bottom-sheet-tag-container">
+					{tags?.map((tagItem, tagIndex) => (
+						<div key={tagItem} className={`profile-bottom-sheet-tag-item ${tagIndex === 0 ? '' : 'ml12'}`}>
+							{tagItem}
+						</div>
+					))}
 				</div>
-				<div className="profile-bottom-sheet-hashtag-bold-text">#{grade}학년&nbsp;</div>
-				<div className="profile-bottom-sheet-hashtag-bold-text">#{status}</div>
-			</div>
-			<div className="profile-bottom-sheet-link-container">
-				{links?.map((linkItem, linkIndex) => (
-					<div key={linkItem} className={linkIndex === 0 ? '' : 'mt8'}>
-						<ProfileLink link={linkItem} />
+				<div className="profile-bottom-sheet-divider" />
+				<div className="profile-bottom-sheet-hashtag-container">
+					<div className="profile-bottom-sheet-hashtag-bold-text">
+						#{dept} <span className="profile-bottom-sheet-hashtag-regular-text">전공</span>&nbsp;
 					</div>
-				))}
+					<div className="profile-bottom-sheet-hashtag-bold-text">#{grade}학년&nbsp;</div>
+					<div className="profile-bottom-sheet-hashtag-bold-text">#{status}</div>
+				</div>
+				<div className="profile-bottom-sheet-link-container">
+					{links?.map((linkItem, linkIndex) => (
+						<div key={linkItem} className={linkIndex === 0 ? '' : 'mt8'}>
+							<ProfileLink link={linkItem} />
+						</div>
+					))}
+				</div>
+				<div className="profile-bottom-sheet-divider" />
+				<div className="profile-bottom-sheet-about-title">저는요..</div>
+				<div className="profile-bottom-sheet-about-text">{userAbout}</div>
 			</div>
-			<div className="profile-bottom-sheet-divider" />
-			<div className="profile-bottom-sheet-about-title">저는요..</div>
-			<div className="profile-bottom-sheet-about-text">{userAbout}</div>
-		</div>
-	);
+		);
+	};
 
 	if (windowWidth > 1024) {
 		return (
@@ -76,7 +89,14 @@ const ProfilePopupPresenter = ({
 		);
 	}
 	return (
-		<BottomSheet open={profileSheetVisible} onDismiss={() => setProfileSheetVisible(false)}>
+		<BottomSheet
+			ref={bottomSheetRef}
+			open={profileSheetVisible}
+			onDismiss={() => {
+				setProfileSheetVisible(false);
+				setBottomSheetHeight(BOTTOMSHEET_MINHEIGHT);
+			}}
+		>
 			<Content />
 		</BottomSheet>
 	);

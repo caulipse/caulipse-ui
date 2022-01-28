81e33c3a5b111ba832d9da33ca5e8da8fc9fff24
Jeyoung-Park <jason9380@naver.com>
2022-01-28 22:44:58 +0900

    fix: 스터디 정보 반응형 이슈 수정

--- src/app/studyDetail/studyContent/info/StudyInfoContentPresenter.tsx
+++ src/app/studyDetail/studyContent/info/StudyInfoContentPresenter.tsx
@@ -7,18 +7,20 @@ interface StudyInfoContentPresenterProps {
 }
 const StudyInfoContentPresenter = ({ study }: StudyInfoContentPresenterProps): JSX.Element => (
 	<div className="content">
-		<div className="studyTimeViewedAnd">
-			<div>{study.createdAt}</div>
-			<div className="mh5">•</div>
-			<div>조회 {study.views}</div>
-			<div className="mh5">•</div>
-			<div>북마크 {NumberUtils.toFormattedCount(study.bookmarks ?? 0)}</div>
-		</div>
-		<div className="studyTitle">
-			<span>{study.title}</span>
-		</div>
-		<div className="studyContent">
-			<span>{study.studyAbout}</span>
+		<div className="mh20">
+			<div className="studyTimeViewedAnd">
+				<div>{study.createdAt}</div>
+				<div className="mh5">•</div>
+				<div>조회 {study.views}</div>
+				<div className="mh5">•</div>
+				<div>북마크 {NumberUtils.toFormattedCount(study.bookmarks ?? 0)}</div>
+			</div>
+			<div className="studyTitle">
+				<span>{study.title}</span>
+			</div>
+			<div className="studyContent">
+				<span>{study.studyAbout}</span>
+			</div>
 		</div>
 	</div>
 );

055aa1dd059bb521a560c09d9b46b7e6881adf10
Jeyoung-Park <jason9380@naver.com>
2022-02-03 23:50:57 +0900

    feat: 로그인 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import "./App.scss";
-import { Footer, Header } from "./app/shared/components";
-import ProfilePage from "./pages/Profile";
-import StudyPage from "./pages/Study";
-import StudyDetailPage from "./pages/StudyDetail";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import './App.scss';
+import { Footer, Header } from './app/shared/components';
+import LoginPage from './pages/Login';
+import ProfilePage from './pages/Profile';
+import StudyPage from './pages/Study';
+import StudyDetailPage from './pages/StudyDetail';
 
 const MainContainer = (): JSX.Element => (
-  <div className="router-con">
-    <div className="main-con">
-        <Switch>
-          <Route exact path="/study/:category" component={StudyPage}/>
-          <Route path="/profile" component={ProfilePage}/>
-          <Route path="/study/detail/:studyId" component={StudyDetailPage}/>
-          <Redirect path="*" to="/study/employment"/>
-        </Switch>
-    </div>
-  </div>
+	<div className="router-con">
+		<div className="main-con">
+			<Switch>
+				<Route exact path="/study/:category" component={StudyPage} />
+				<Route path="/profile" component={ProfilePage} />
+				<Route path="/login" component={LoginPage} />
+				<Route path="/study/detail/:studyId" component={StudyDetailPage} />
+				<Redirect path="*" to="/study/employment" />
+			</Switch>
+		</div>
+	</div>
 );
 
 const App = (): JSX.Element => {
-  return (
-    <Router>
-      <Header/>
-      <MainContainer/>
-      <Footer/>
-    </Router>
-  );
+	return (
+		<Router>
+			<Header />
+			<MainContainer />
+			<Footer />
+		</Router>
+	);
 };
 
 export default App;

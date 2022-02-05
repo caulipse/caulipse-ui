76bb6c406099512c4ec26a93cf562072891e04ae
Jeyoung-Park <jason9380@naver.com>
2022-02-06 00:04:57 +0900

    feat: 더미 데이터 생성 및 리덕스

--- src/app/profile/my/notice/NoticeContainer.tsx
+++ src/app/profile/my/notice/NoticeContainer.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import { useAppDispatch, useAppSelector } from '@src/hooks/appRedux';
+import React, { useEffect } from 'react';
+import * as Factory from 'factory.ts';
+import { NoticeInterface, setNotices } from '@src/store/modules/user';
 import NoticePresenter from './NoticePresenter';
 
-const NoticeContainer=():JSX.Element=>{
-  return <NoticePresenter />;
-}
+const NoticeContainer = (): JSX.Element => {
+	const dispatch = useAppDispatch();
+	const notices = useAppSelector((state) => state.user.notices);
+
+	const getNotices = (iter: number) => {
+		const appliedStudiesFactory = Factory.Sync.makeFactory<NoticeInterface>({
+			noticeId: Factory.each((i) => i.toString()),
+			title: '제목입니다.',
+			content:
+				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+			createdAt: new Date(),
+		});
+		return appliedStudiesFactory.buildList(iter);
+	};
+
+	useEffect(() => {
+		dispatch(setNotices({ notices: getNotices(5) }));
+	}, []);
+
+	return <NoticePresenter notices={notices} />;
+};
 
 export default NoticeContainer;
--- src/app/profile/my/notice/NoticePresenter.tsx
+++ src/app/profile/my/notice/NoticePresenter.tsx
@@ -1,6 +1,12 @@
+import { NoticeInterface } from '@src/store/modules/user';
 import React from 'react';
 
-const NoticePresenter = (): JSX.Element => {
+interface NoticePresenterProps {
+	notices: NoticeInterface[];
+}
+
+const NoticePresenter = ({ notices }: NoticePresenterProps): JSX.Element => {
+	console.log('notices, ', notices);
 	return <div>NoticePresenter</div>;
 };
 

f022b3935e6592e9949e9d3180d280927ff0f8d4
Jeyoung-Park <jason9380@naver.com>
2022-02-07 01:49:49 +0900

    feat: notice header

--- src/app/profile/my/notice/NoticePresenter.tsx
+++ src/app/profile/my/notice/NoticePresenter.tsx
@@ -1,13 +1,27 @@
 import { NoticeInterface } from '@src/store/modules/user';
 import React from 'react';
+import './styles.scss';
+import { IoArrowBack } from 'react-icons/io5';
+import { useHistory } from 'react-router-dom';
 
 interface NoticePresenterProps {
 	notices: NoticeInterface[];
 }
 
 const NoticePresenter = ({ notices }: NoticePresenterProps): JSX.Element => {
-	console.log('notices, ', notices);
-	return <div>NoticePresenter</div>;
+	const history = useHistory();
+
+	return (
+		<>
+			<div className="notice-presenter-header-container">
+				<button type="button" onClick={() => history.goBack()}>
+					<IoArrowBack size={24} color="#f7f7f7" />
+				</button>
+				<div className="notice-presenter-header-title">공지사항</div>
+				<div />
+			</div>
+		</>
+	);
 };
 
 export default NoticePresenter;

cb23c6ca3073f76ad53b807bbcebd088c4da9f0d
Jeyoung-Park <jason9380@naver.com>
2022-02-07 23:46:21 +0900

    feat: notice detail 구조

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MyPage from './my/MyPage';
 import MyProfileEditPage from './myProfileEdit/MyProfileEditPage';
+import NoticeDetailPage from './notice/NoticeDetailPage';
 import NoticePage from './notice/NoticePage';
 import ProfileStudies from './studies';
 
@@ -9,6 +10,7 @@ const ProfileContainer = (): JSX.Element => (
 	<Switch>
 		<Route exact path="/profile/edit" component={MyProfileEditPage} />
 		<Route exact path="/profile/notice" component={NoticePage} />
+		<Route exact path="/profile/notice/:noticeId" component={NoticeDetailPage} />
 		<Route path="/profile/studies" component={ProfileStudies} />
 		<Route exact path="/profile/:userId" component={MyPage} />
 	</Switch>
---
+++ src/pages/Profile/notice/NoticeDetailPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NoticeDetailPage = (): JSX.Element => {
+	return <div>NoticeDetailPage</div>;
+};
+
+export default NoticeDetailPage;

bf3ef4034ad48d5a0e4f05fa7bf47362b4d67c8f
Jeyoung-Park <jason9380@naver.com>
2022-02-15 02:43:05 +0900

    fix: container presenter 생성

---
+++ src/app/profile/my/accountInfo/AccountInfoPresenter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface AccountInfoPresenterProps {
+	userId: string;
+}
+
+const AccountInfoPresenter = ({ userId }: AccountInfoPresenterProps): JSX.Element => {
+	return <div>AccountInfoPresenter</div>;
+};
+
+export default AccountInfoPresenter;
--- src/pages/Profile/accountInfo/AccountInfoPage.tsx
+++ src/pages/Profile/accountInfo/AccountInfoPage.tsx
@@ -1,7 +1,12 @@
+import AccountInfoContainer from '@src/app/profile/my/accountInfo/AccountInfoContainer';
 import React from 'react';
 
-const AccountInfoPage = ():JSX.Element => {
-	return <div>AccountInfoPage</div>;
+const AccountInfoPage = (): JSX.Element => {
+	return (
+		<div>
+			<AccountInfoContainer />
+		</div>
+	);
 };
 
 export default AccountInfoPage;

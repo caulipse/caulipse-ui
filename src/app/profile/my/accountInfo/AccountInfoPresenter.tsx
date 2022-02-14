22ba3607cad2996db8c26d10052566035e4ebc92
Jeyoung-Park <jason9380@naver.com>
2022-02-15 02:51:58 +0900

    feat: 계정 정보 헤더

--- src/app/profile/my/accountInfo/AccountInfoPresenter.tsx
+++ src/app/profile/my/accountInfo/AccountInfoPresenter.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import './styles.scss';
+import { Button } from '@material-ui/core';
+import { IoArrowBack } from 'react-icons/io5';
+import { useHistory } from 'react-router-dom';
 
 interface AccountInfoPresenterProps {
 	userId: string;
 }
 
 const AccountInfoPresenter = ({ userId }: AccountInfoPresenterProps): JSX.Element => {
-	return <div>AccountInfoPresenter</div>;
+	const history = useHistory();
+
+	return (
+		<div>
+			<div className="account-info-header-container">
+				<button type="button" onClick={() => history.goBack()}>
+					<IoArrowBack color="#f2f2f2" size={24} />
+				</button>
+				<div className="account-info-header-title">계정 정보</div>
+				<div />
+			</div>
+		</div>
+	);
 };
 
 export default AccountInfoPresenter;

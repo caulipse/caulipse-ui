301f7d5dd5fe4573f5df7cafefce16575d10de88
taechonkim <mirijo02233092@gmail.com>
2022-02-20 18:59:40 +0900

    feat: 공통적으로 사용되는 리소스에 대한 타입을 types.ts 파일로 분리

--- src/api/response/study.ts
+++ src/api/response/study.ts
@@ -1,35 +1,4 @@
-export interface Category {
-	code: number;
-	main: string;
-	sub: string;
-}
-
-export interface Host {
-	email: string;
-	id: string;
-	isLogout: boolean;
-	password: string;
-	role: string;
-	token: string;
-}
-
-export interface Study {
-	HOST_ID: string;
-	capacity: number;
-	categoryCode?: Category;
-	createdAt: string;
-	frequency: string;
-	hostId?: Host;
-	id: string;
-	isOpen: boolean;
-	location: string;
-	membersCount: number;
-	studyAbout: string;
-	title: string;
-	vacancy: number;
-	views: number;
-	weekday: string;
-}
+import { Study } from '@api/types';
 
 export interface IResponseGetStudies {
 	message: string;
---
+++ src/api/types.ts
@@ -0,0 +1,32 @@
+export interface Category {
+	code: number;
+	main: string;
+	sub: string;
+}
+
+export interface Host {
+	email: string;
+	id: string;
+	isLogout: boolean;
+	password: string;
+	role: string;
+	token: string;
+}
+
+export interface Study {
+	HOST_ID: string;
+	capacity: number;
+	categoryCode?: Category;
+	createdAt: string;
+	frequency: string;
+	hostId?: Host;
+	id: string;
+	isOpen: boolean;
+	location: string;
+	membersCount: number;
+	studyAbout: string;
+	title: string;
+	vacancy: number;
+	views: number;
+	weekday: string;
+}

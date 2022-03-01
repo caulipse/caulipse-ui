84c78479a303cdd29ab736caf43ff1e334cd9931
taechonkim <mirijo02233092@gmail.com>
2022-03-01 11:15:49 +0900

    feat: CheckIcon 컴포넌트 추가

---
+++ src/components/common/icon/CheckIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { IoCheckmarkSharp } from 'react-icons/io5';
+
+const CheckIcon = () => {
+	return <IoCheckmarkSharp size={24} />;
+};
+
+export default CheckIcon;

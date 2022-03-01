5fbd17c457df7d2376300861a2ceecf8c024fe88
taechonkim <mirijo02233092@gmail.com>
2022-03-01 11:46:29 +0900

    feat: 링크복사 기능 개발, 스낵바 적용

--- src/pages/studyDetail/index.tsx
+++ src/pages/studyDetail/index.tsx
@@ -3,6 +3,7 @@ import StudyInfoContainer from '@src/app/studyDetail/studyInfo/StudyInfoContaine
 import React, { useEffect, useRef, useState } from 'react';
 import { IoArrowBack, IoBookmarkOutline, IoEllipsisVertical, IoShareSocialOutline } from 'react-icons/io5';
 import { useHistory, useParams } from 'react-router-dom';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './styles.scss';
 import ApplyModalContainer from '@src/app/studyDetail/studyContent/modal/applyModal/ApplyModalContainer';
 import UserStudyMoreModalContainer from '@studyDetail/studyContent/modal/userStudyMoreModal/UserStudyMoreModalContainer';
@@ -14,6 +15,7 @@ import ApplyCancelModalContainer from '@src/app/studyDetail/studyContent/modal/a
 import EditCategoryModalContainer from '@src/app/studyDetail/studyContent/modal/editCategoryModal/EditCategoryModalContainer';
 import useFetchStudy from '@src/hooks/remotes/study/useFetchStudy';
 import Loader from '@src/components/common/loader/Loader';
+import useSnackbar from '@src/hooks/snackbar/useSnackbar';
 
 const StudyDetailPage = (): JSX.Element => {
 	const history = useHistory();
@@ -30,6 +32,8 @@ const StudyDetailPage = (): JSX.Element => {
 	const [openCancelModal, setOpenCancelModal] = useState<boolean>(false);
 	const [openEditCategoryModal, setOpenEditCategoryModal] = useState<boolean>(false);
 
+	const { openSnackbar } = useSnackbar();
+
 	const onClick = () => {
 		setOpenApplyModal(!openApplyModal);
 	};
@@ -43,6 +47,12 @@ const StudyDetailPage = (): JSX.Element => {
 		} else {
 			setOpenUserMoreModal(!openUserMoreModal);
 		}
+	};
+
+	const url = window.location.href;
+
+	const onClickShare = () => {
+		openSnackbar('클립보드에 복사되었습니다.');
 	};
 
 	return (
@@ -60,9 +70,11 @@ const StudyDetailPage = (): JSX.Element => {
 								<button type="button" className="mr11">
 									<IoBookmarkOutline size={16} color="#ffffff" />
 								</button>
-								<button type="button" className="mr11">
-									<IoShareSocialOutline size={16} color="#ffffff" />
-								</button>
+								<CopyToClipboard text={url}>
+									<button type="button" className="mr11" onClick={onClickShare}>
+										<IoShareSocialOutline size={16} color="#ffffff" />
+									</button>
+								</CopyToClipboard>
 								<button type="button" onClick={onClickMore}>
 									<IoEllipsisVertical size={16} color="#ffffff" />
 								</button>

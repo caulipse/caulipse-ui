9712a7af9d87c6b28aa99cf50767b4a3b96bd82a
taechonkim <mirijo02233092@gmail.com>
2022-03-04 22:40:57 +0900

    refactor: 논의된 아이콘으로 변경

--- src/components/common/icon/CloseIcon.tsx
+++ src/components/common/icon/CloseIcon.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { IoCloseOutline } from 'react-icons/io5';
+import { IoClose } from 'react-icons/io5';
 import classnames from 'classnames';
 import './index.scss';
 import { IconAlignEnum, IIconProps } from './types';
 
 const CloseIcon = ({ onClick, align = IconAlignEnum.right }: IIconProps) => {
 	return (
-		<IoCloseOutline
+		<IoClose
 			className={classnames('close-icon', { 'align-left': align === IconAlignEnum.left })}
 			onClick={onClick}
 			size={24}

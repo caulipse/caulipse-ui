9455a14021728efc0ebf44d5932790f65bc26ab4
taechonkim <mirijo02233092@gmail.com>
2022-03-05 10:57:06 +0900

    feat: secondary type chip component 추가

--- src/components/common/chip/Chip.tsx
+++ src/components/common/chip/Chip.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import classnames from 'classnames';
-import { IChipProps } from './types';
+import { IChipProps, ChipTypeEnum } from './types';
 import './index.scss';
 
-const Chip = ({ selected, label, onClick }: IChipProps): JSX.Element => {
+const Chip = ({ selected, label, onClick, type = ChipTypeEnum.primary }: IChipProps): JSX.Element => {
 	const handleClick = () => {
 		onClick(label);
 	};
 	return (
-		<Container onClick={handleClick} className={classnames('chip-container', { selected })}>
+		<Container
+			onClick={handleClick}
+			className={classnames('chip-container', { selected }, { 'secondary-chip-container': ChipTypeEnum.secondary })}
+		>
 			{label}
 		</Container>
 	);

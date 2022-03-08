a84bbf075dba8c58eaba70341363445d620e7f10
Jeyoung-Park <jason9380@naver.com>
2022-03-09 03:36:21 +0900

    fix: 카테고리 라벨 get하는 함수

---
+++ src/app/shared/utils/category.ts
@@ -0,0 +1,11 @@
+import categories, { CategoryType } from '@src/const';
+
+// eslint-disable-next-line import/prefer-default-export
+export const getCategoryName = (categoryCode: number): string => {
+	let result: CategoryType = { label: '', code: 0 };
+	categories.forEach((categoryItem) => {
+		const filteredData = categoryItem.subCategories.find((subCategoryItem) => subCategoryItem.code === categoryCode);
+		if (filteredData) result = filteredData;
+	});
+	return result.label;
+};

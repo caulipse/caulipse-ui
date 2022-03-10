18917856bda72abb6aea5b93edbc14cb26f0b566
Jeyoung-Park <jason9380@naver.com>
2022-03-10 23:17:29 +0900

    fix: logout 성공 시 홈으로 리디렉션

--- src/hooks/remotes/user/usePatchLogout.ts
+++ src/hooks/remotes/user/usePatchLogout.ts
@@ -1,8 +1,11 @@
 import API from '@src/api';
 import { useMutation, useQuery } from 'react-query';
+import { useHistory } from 'react-router-dom';
 import QUERY_KEY from '..';
 
 export default () => {
+	const history = useHistory();
+
 	const mutation = async (): Promise<any> => {
 		const res = await API.logout();
 		return res.data;
@@ -10,6 +13,7 @@ export default () => {
 	return useMutation(QUERY_KEY.LOGOUT, mutation, {
 		onSuccess: (response: any) => {
 			console.log(response);
+			history.push('/');
 		},
 		onError: (e: Error) => {
 			console.error(e.message);

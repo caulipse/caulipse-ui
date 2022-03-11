dca8f698c23b5a46f393441711fcaafa86cc7d6d
Jeyoung-Park <jason9380@naver.com>
2022-03-12 02:03:43 +0900

    fix: 로그인 전 헤더 ui  추가

--- src/app/shared/components/header/index.tsx
+++ src/app/shared/components/header/index.tsx
@@ -1,31 +1,35 @@
+import { Button, Typography } from '@material-ui/core';
 import React from 'react';
+import { IoMenu } from 'react-icons/io5';
 import { useHistory } from 'react-router-dom';
 import { HeaderButtonProps, headerButtons } from './headerList';
 import './index.scss';
 
 const Header: React.FC = () => {
-  const history = useHistory();
-  
-  return (
-    <header className="header-con">
-      <div className="header-wrap">
-        <div>
-          <button type="button" onClick={() => history.push("/study/employment")}>
-            <div className="header-logo">Caulipse</div>
-          </button>
-        </div>
-        <div className="header-user-bt">
-          {
-            headerButtons.map((button: HeaderButtonProps) => (
-              <button key={button.title} type="button" onClick={() => history.push(button.route)}>
-                <div>{button.title}</div>
-              </button>
-            ))
-          }
-        </div>
-      </div>
-    </header>
-  );
+	const history = useHistory();
+
+	const openDrawer = () => {
+		console.log('openDrawer');
+	};
+
+	return (
+		<header className="header-con">
+			<Button onClick={openDrawer}>
+				<IoMenu className="header-menu" />
+			</Button>
+			<Typography>서비스 로고</Typography>
+			<Button onClick={() => history.push('/login')}>
+				<Typography className="header-login">로그인</Typography>
+			</Button>
+			{/* <div className="header-user-bt">
+				{headerButtons.map((button: HeaderButtonProps) => (
+					<button key={button.title} type="button" onClick={() => history.push(button.route)}>
+						<div>{button.title}</div>
+					</button>
+				))}
+			</div> */}
+		</header>
+	);
 };
 
 export default Header;

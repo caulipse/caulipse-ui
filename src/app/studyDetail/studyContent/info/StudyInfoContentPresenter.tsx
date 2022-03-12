723c2eb2595c7cfe14f1c2019d1ecee0b5c518fc
Jeyoung-Park <jason9380@naver.com>
2022-03-13 04:33:11 +0900

    feat: 마감된 스터디 마크업

--- src/app/studyDetail/studyContent/info/StudyInfoContentPresenter.tsx
+++ src/app/studyDetail/studyContent/info/StudyInfoContentPresenter.tsx
@@ -19,28 +19,37 @@ const StudyInfoContentPresenter = ({
 	// bookmarks,
 	title,
 	studyAbout,
-}: StudyInfoContentPresenterProps): JSX.Element => (
-	<div className="content">
-		<div className="mh20">
-			<div className="diff-date-container">
-				<div className="diff-date-d-day">D-{differenceInDays(endDate, new Date())}</div>
-				<div className="diff-date-text">모집마감까지 ({format(endDate, 'MM.dd')})</div>
-			</div>
-			<div className="studyTimeViewedAnd">
-				<div>{format(new Date(createdAt), 'yyyy-MM-dd')}</div>
-				<div className="mh5">•</div>
-				<div>조회 {views}</div>
-				<div className="mh5">•</div>
-				<div>북마크 {NumberUtils.toFormattedCount(0)}</div>
-			</div>
-			<div className="studyTitle">
-				<span>{title}</span>
-			</div>
-			<div className="studyContent">
-				<span>{studyAbout}</span>
+}: StudyInfoContentPresenterProps): JSX.Element => {
+	const dDay = differenceInDays(endDate, new Date());
+
+	return (
+		<div className="content">
+			<div className="mh20">
+				{dDay >= 0 ? (
+					<div className="diff-date-container">
+						<div className="diff-date-d-day">D-{dDay}</div>
+						<div className="diff-date-text">모집마감까지 ({format(endDate, 'MM.dd')})</div>
+					</div>
+				) : (
+					<div className="diff-date-end">마감된 스터디입니다</div>
+				)}
+
+				<div className="studyTimeViewedAnd">
+					<div>{format(new Date(createdAt), 'yyyy-MM-dd')}</div>
+					<div className="mh5">•</div>
+					<div>조회 {views}</div>
+					<div className="mh5">•</div>
+					<div>북마크 {NumberUtils.toFormattedCount(0)}</div>
+				</div>
+				<div className="studyTitle">
+					<span>{title}</span>
+				</div>
+				<div className="studyContent">
+					<span>{studyAbout}</span>
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default StudyInfoContentPresenter;

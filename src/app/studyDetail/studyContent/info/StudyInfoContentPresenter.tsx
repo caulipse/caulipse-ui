7ec6bec011ac15cf1957926a7cb9726d868b513d
Jeyoung-Park <jason9380@naver.com>
2022-03-13 04:02:07 +0900

    fix: 모집 마감 d day 추가

--- src/app/studyDetail/studyContent/info/StudyInfoContentPresenter.tsx
+++ src/app/studyDetail/studyContent/info/StudyInfoContentPresenter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NumberUtils from '@src/app/shared/utils/number';
-import format from 'date-fns/format';
+import { differenceInDays, format } from 'date-fns';
+import '../styles.scss';
 
 interface StudyInfoContentPresenterProps {
 	createdAt: string;
@@ -9,6 +10,9 @@ interface StudyInfoContentPresenterProps {
 	title: string;
 	studyAbout: string;
 }
+
+const endDate = new Date(2023, 0, 1);
+
 const StudyInfoContentPresenter = ({
 	createdAt,
 	views,
@@ -18,6 +22,10 @@ const StudyInfoContentPresenter = ({
 }: StudyInfoContentPresenterProps): JSX.Element => (
 	<div className="content">
 		<div className="mh20">
+			<div className="diff-date-container">
+				<div className="diff-date-d-day">D-{differenceInDays(endDate, new Date())}</div>
+				<div className="diff-date-text">모집마감까지 ({format(endDate, 'MM.dd')})</div>
+			</div>
 			<div className="studyTimeViewedAnd">
 				<div>{format(new Date(createdAt), 'yyyy-MM-dd')}</div>
 				<div className="mh5">•</div>

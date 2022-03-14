63c30be9d65a7000a4bb358d72deafed9695ca24
Jeyoung-Park <jason9380@naver.com>
2022-03-15 01:23:11 +0900

    feat: useFetchBookmarks

---
+++ src/hooks/remotes/bookmark/useFetchBookmarks.ts
@@ -0,0 +1,15 @@
+import API from '@src/api';
+import { useQuery } from 'react-query';
+import QUERY_KEY from '..';
+
+export default () => {
+	const fetcher = async (): Promise<any> => {
+		const res = await API.getBookmarks();
+		return res.data;
+	};
+	return useQuery(QUERY_KEY.FETCH_BOOKMARKS, fetcher, {
+		onError: (e) => {
+			console.log(e);
+		},
+	});
+};

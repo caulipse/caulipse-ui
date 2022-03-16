a31894315d9a9f53288fa22f00faa3ea3075d81b
Jeyoung-Park <jason9380@naver.com>
2022-03-17 00:13:35 +0900

    fix: data fetch

--- src/app/profile/my/notice/NoticeDetailPresenter.tsx
+++ src/app/profile/my/notice/NoticeDetailPresenter.tsx
@@ -1,19 +1,19 @@
+import { Notice } from '@src/api/types';
 import { format } from 'date-fns';
 import React from 'react';
-import { NoticeInterface } from '../../interface/interface';
 import './styles.scss';
 
 interface NoticeDetailPresenterProps {
-	notice: NoticeInterface;
+	notice: Notice;
 }
 
 const NoticeDetailPresenter = ({ notice }: NoticeDetailPresenterProps): JSX.Element => {
 	return (
 		<div className="notice-detail-container">
 			<div className="notice-detail-content-container">
-				<div className="notice-detail-created-at">{format(notice.createdAt, 'yyyy-MM-dd')}</div>
+				<div className="notice-detail-created-at">{format(new Date(notice.createdAt), 'yyyy-MM-dd')}</div>
 				<div className="notice-detail-title">{notice.title}</div>
-				<div className="notice-detail-content">{notice.content}</div>
+				<div className="notice-detail-content">{notice.about}</div>
 			</div>
 		</div>
 	);

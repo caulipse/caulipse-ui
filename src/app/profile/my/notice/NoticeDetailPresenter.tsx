934b5fcb7c93fbe82821584b4839e73a8e021af9
Jeyoung-Park <jason9380@naver.com>
2022-03-17 00:08:09 +0900

    fix: noticeDetail header 삭제

--- src/app/profile/my/notice/NoticeDetailPresenter.tsx
+++ src/app/profile/my/notice/NoticeDetailPresenter.tsx
@@ -1,7 +1,5 @@
 import { format } from 'date-fns';
 import React from 'react';
-import { IoArrowBack } from 'react-icons/io5';
-import { useHistory } from 'react-router-dom';
 import { NoticeInterface } from '../../interface/interface';
 import './styles.scss';
 
@@ -10,15 +8,8 @@ interface NoticeDetailPresenterProps {
 }
 
 const NoticeDetailPresenter = ({ notice }: NoticeDetailPresenterProps): JSX.Element => {
-	const history = useHistory();
-
 	return (
 		<div className="notice-detail-container">
-			<div className="notice-detail-header">
-				<button type="button" onClick={() => history.goBack()}>
-					<IoArrowBack size={24} color="#f7f7f7" />
-				</button>
-			</div>
 			<div className="notice-detail-content-container">
 				<div className="notice-detail-created-at">{format(notice.createdAt, 'yyyy-MM-dd')}</div>
 				<div className="notice-detail-title">{notice.title}</div>

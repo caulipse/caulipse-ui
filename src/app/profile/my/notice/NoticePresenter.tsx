7f46def95fd843b8e0021f91d03e84ab8bf22b32
Jeyoung-Park <jason9380@naver.com>
2022-03-17 00:17:47 +0900

    fix: notice list 헤더 삭제

--- src/app/profile/my/notice/NoticePresenter.tsx
+++ src/app/profile/my/notice/NoticePresenter.tsx
@@ -10,17 +10,8 @@ interface NoticePresenterProps {
 }
 
 const NoticePresenter = ({ notices }: NoticePresenterProps): JSX.Element => {
-	const history = useHistory();
-
 	return (
 		<>
-			<div className="notice-presenter-header-container">
-				<button type="button" onClick={() => history.goBack()}>
-					<IoArrowBack size={24} color="#f7f7f7" />
-				</button>
-				<div className="notice-presenter-header-title">공지사항</div>
-				<div />
-			</div>
 			<div className="notice-presenter-list-container">
 				<div className="notice-presenter-list-max-width">
 					{notices.map((item) => (

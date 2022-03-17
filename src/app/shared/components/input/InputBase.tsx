2ddbda65217716e244c7e9e91459c0ab1d7fceae
Jeyoung-Park <jason9380@naver.com>
2022-03-18 01:04:25 +0900

    feat: focus 시에만 등록 버튼 나오도록 수정

--- src/app/shared/components/input/InputBase.tsx
+++ src/app/shared/components/input/InputBase.tsx
@@ -1,32 +1,50 @@
-import { Input } from '@material-ui/core';
-import React, { useRef } from 'react';
+import { Box, Button, Input } from '@material-ui/core';
+import React, { useRef, useState } from 'react';
 import './inputBase.scss';
 
 const sampleImgUrl = 'https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__480.jpg';
+const MAX_LENGTH = 500;
 
 interface InputBaseProps {
 	placeholder: string;
 	content: string;
 	setContent: (content: string) => void;
 }
+
 const InputBase = ({ placeholder, content, setContent }: InputBaseProps): JSX.Element => {
+	const [isFocused, setIsFocused] = useState<boolean>(false);
+
 	const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setContent(event.target.value);
 	};
 
 	return (
-		<div className="inputbase-container">
+		<Box className="inputbase-container">
 			<img className="inputbase-img" src={sampleImgUrl} alt="프로필 이미지" width={40} height={40} />
-			<Input
-				className="inputbase-input"
-				disableUnderline
-				multiline
-				placeholder={placeholder}
-				value={content}
-				onChange={onChange}
-				inputProps={{ maxLength: 500 }}
-			/>
-		</div>
+			<Box className="inputbase-column-container">
+				<Input
+					className="inputbase-input"
+					disableUnderline
+					multiline
+					placeholder={placeholder}
+					value={content}
+					onChange={onChange}
+					inputProps={{ maxLength: MAX_LENGTH }}
+					onFocus={() => setIsFocused(true)}
+					onBlur={() => setIsFocused(false)}
+				/>
+				{isFocused && (
+					<Box className="inputbase-submit-container">
+						<Box className="inputbase-text-length">
+							{content.length}/{MAX_LENGTH}
+						</Box>
+						<Button className="inputbase-submit-btn" variant="contained" size="small" disableElevation>
+							등록
+						</Button>
+					</Box>
+				)}
+			</Box>
+		</Box>
 	);
 };
 

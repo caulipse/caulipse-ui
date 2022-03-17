7a107fe1c142fcbc90875834e09b9f6a06683aab
tngur1101 <tngur1101@naver.com>
2022-03-17 23:18:12 +0900

    pr

--- src/app/signup/boxcontainer/SignupBoxContainer.tsx
+++ src/app/signup/boxcontainer/SignupBoxContainer.tsx
@@ -6,8 +6,8 @@ const SignupBoxContainer = (): JSX.Element => {
         <div>
             <div className="portal">
                 <form>
-                    <input type="text" className="portal-id" value="ID" placeholder="포탈 아이디"/>
-                    <input type="password" className="portal-pd" value="password" placeholder="비밀번호 (특수문자를 포함한 8자 이상)"/>
+                    <input type="text" className="signportal-id" value="ID" placeholder="포탈 아이디"/>
+                    <input type="password" className="signportal-pd" value="password" placeholder="비밀번호 (특수문자를 포함한 8자 이상)"/>
                 </form>
                 <button type="button" className="signup-btn">가입하기</button>
                 <button type="button" className="forgotten-btn">비밀번호를 잊어버리셨나요?</button>

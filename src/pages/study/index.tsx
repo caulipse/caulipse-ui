e24be51ec774b60f6ce4180dde67a957b0c7b57d
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 <mirijo02233092@gmail.com>
2022-03-21 22:44:54 +0900

    feat: 스터디 리스트 페이지 모바일/웹 분기 처리

--- src/pages/study/index.tsx
+++ src/pages/study/index.tsx
@@ -1,71 +1,15 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import StudyListContainter from '@src/app/study/studyList/StudyListContainer';
-import StudyCreateButton from '@study/studyCreateButton/StudyCreateButton';
-import MainCategoryContainer from '@study/mainCategory/MainCategoryContainer';
-import StudySortFilterContainer from '@study/studySortFilter/StudySortFilterContainer';
-import SubCategoryContainer from '@src/app/study/subCategory/SubCategoryContainer';
-import SubCategoryCollapsedPresenter from '@src/app/study/subCategory/SubCategoryCollapsedPresenter';
-import { useAtom } from 'jotai';
-import { studyListState } from '@src/state';
-import { MainCategoryType, CategoryType } from '@src/types';
+import React from 'react';
 import { Container } from '@material-ui/core';
+import MobileStudyPage from '@src/app/study/mobile/MobileStudyPage';
+import DesktopStudyPage from '@src/app/study/desktop/DesktopStudyPage';
+import useWindowDimensions from '@src/hooks/useWindowDimensions';
 import './index.scss';
 
-const StudyPage = (): JSX.Element => {
-	const [mainCategory, setMainCategory] = useState<MainCategoryType>();
-	const [state, setState] = useAtom(studyListState);
+const MainPage = (): JSX.Element => {
+	const { width } = useWindowDimensions();
+	const desktop = width > 1024;
 
-	const { selectedSubCategories } = state;
-
-	const onClickCreate = () => {
-		// TODO
-		// 스터디 등록 모달 연결
-	};
-
-	useEffect(() => {
-		// TODO
-		// API 연동
-		setState({ ...state, selectedSubCategories: [] as CategoryType[] });
-	}, [mainCategory]);
-
-	const element = useRef(null);
-	const [collapse, setCollapse] = useState(false);
-
-	const onScroll = useCallback(([entry]) => {
-		setCollapse(!entry.isIntersecting);
-	}, []);
-
-	useEffect(() => {
-		let observer: any;
-
-		if (element.current) {
-			observer = new IntersectionObserver(onScroll, { threshold: 1 });
-			observer.observe(element.current);
-		}
-
-		return () => observer && observer.disconnect();
-	}, [onScroll]);
-
-	const onClick = (category: CategoryType) => {
-		setState({ ...state, selectedSubCategories: selectedSubCategories.filter((item) => item !== category) });
-	};
-
-	return (
-		<Container className="study-list-container ">
-			<MainCategoryContainer onChange={setMainCategory} />
-			{collapse ? (
-				selectedSubCategories?.length && (
-					<SubCategoryCollapsedPresenter selectedSubCategories={selectedSubCategories} onClick={onClick} />
-				)
-			) : (
-				<SubCategoryContainer mainCategory={mainCategory} />
-			)}
-			<br ref={element} className="sub-category-presenter-container-ref" />
-			<StudyCreateButton onClick={onClickCreate} />
-			<StudySortFilterContainer />
-			<StudyListContainter />
-		</Container>
-	);
+	return <Container className="study-page-container">{desktop ? <DesktopStudyPage /> : <MobileStudyPage />}</Container>;
 };
 
-export default StudyPage;
+export default MainPage;

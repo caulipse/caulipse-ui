bfa31e5fb6c4f93b741d2b9278f94e900d83a2a5
Jeyoung-Park <jason9380@naver.com>
2022-03-22 00:49:51 +0900

    fix: preview container 수정

--- src/pages/profile/my/MyPage.tsx
+++ src/pages/profile/my/MyPage.tsx
@@ -1,17 +1,10 @@
 import MyBtns from '@src/app/profile/my/btns';
 import MyHeaderContainer from '@src/app/profile/my/header/MyHeaderContainer';
 import PreviewContainer from '@src/app/profile/my/preview/PreviewContainer';
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 import './index.scss';
 
-interface MyPageParamsInterface {
-	userId: string;
-}
-
 const MyPage = (): JSX.Element => {
-	const params = useParams<MyPageParamsInterface>();
-
 	return (
 		<div className="my-page-container">
 			<MyHeaderContainer />

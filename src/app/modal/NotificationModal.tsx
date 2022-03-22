1054103d776b2ef1b3305a6692b20f0a89ebc0b7
Jeyoung-Park <jason9380@naver.com>
2022-03-22 22:41:19 +0900

    feat: 알림이 없는 경우

--- src/app/modal/NotificationModal.tsx
+++ src/app/modal/NotificationModal.tsx
@@ -1,27 +1,37 @@
 import { Box } from '@material-ui/core';
+import Loader from '@src/components/common/loader/Loader';
 import { IModalContainerCommonProps } from '@src/components/common/modal/types';
+import useFetchNotifications from '@src/hooks/remotes/notification/useFetchNotifications';
 import useWindowDimensions from '@src/hooks/useWindowDimensions';
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { BottomSheet } from 'react-spring-bottom-sheet';
 import './notificationModal.scss';
 
 const NotificationModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element => {
 	const { width } = useWindowDimensions();
+	const { data, isLoading } = useFetchNotifications();
+	console.log('data, ', data);
 
 	const isDesktop = useMemo(() => {
 		return width > 1024;
 	}, [width]);
 
+	const renderNotifications = useCallback(() => {
+		if (isLoading) return <Loader />;
+		if (data?.length === 0) {
+			return <Box className="notification-modal-empty-text">알림이 없습니다.</Box>;
+		}
+		return <Box>알림</Box>;
+	}, [isLoading, data]);
+
 	return isDesktop ? (
 		<>
-			<Box className="notification-modal-desktop-container speech-bubble-triangle">
-				<div>알림</div>
-			</Box>
+			<Box className="notification-modal-desktop-container speech-bubble-triangle">{renderNotifications()}</Box>
 			<Box className="speech-bubble-bg" onClick={() => onClose(false)} />
 		</>
 	) : (
-		<BottomSheet open={open} onDismiss={() => onClose(false)} className="modal-bottom-sheet">
-			<div>알림</div>
+		<BottomSheet open={open} onDismiss={() => onClose(false)}>
+			{renderNotifications()}
 		</BottomSheet>
 	);
 };

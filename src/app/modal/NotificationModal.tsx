d46679c7b973e566764c05091331881247906865
Jeyoung-Park <jason9380@naver.com>
2022-03-22 22:21:01 +0900

    feat: speech bubble

--- src/app/modal/NotificationModal.tsx
+++ src/app/modal/NotificationModal.tsx
@@ -1,10 +1,22 @@
+import { Box } from '@material-ui/core';
 import { IModalContainerCommonProps } from '@src/components/common/modal/types';
-import React from 'react';
+import useWindowDimensions from '@src/hooks/useWindowDimensions';
+import React, { useMemo } from 'react';
 import { BottomSheet } from 'react-spring-bottom-sheet';
 import './notificationModal.scss';
 
 const NotificationModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element => {
-	return (
+	const { width } = useWindowDimensions();
+
+	const isDesktop = useMemo(() => {
+		return width > 1024;
+	}, [width]);
+
+	return isDesktop ? (
+		<Box className="notification-modal-desktop-container speech-bubble-triangle">
+			<div>알림</div>
+		</Box>
+	) : (
 		<BottomSheet open={open} onDismiss={() => onClose(false)} className="modal-bottom-sheet">
 			<div>알림</div>
 		</BottomSheet>

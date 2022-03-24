283c465f9202146f695be558f2ed89df7de90a1d
Jeyoung-Park <jason9380@naver.com>
2022-03-25 00:14:30 +0900

    fix: chip ui

--- src/app/profile/appliedStudies/AppliedStudiesPresenter.tsx
+++ src/app/profile/appliedStudies/AppliedStudiesPresenter.tsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react';
 import { IoChevronDown, IoChevronUp } from 'react-icons/io5';
 import { AppliedStudy } from '@src/api/types';
 import './index.scss';
+import MyStudyCard from '@src/app/shared/components/myStudyCard';
+import classNames from 'classnames';
+import { Box } from '@material-ui/core';
 
 interface AppliedStudiesPresenterProps {
 	openedAppliedStudies: AppliedStudy[];
@@ -19,13 +22,40 @@ const AppliedStudiesPresenter = ({
 		console.log('findStudies');
 	};
 
+	const renderOpenedAppliedStudies = () => {
+		return openedAppliedStudies.map((item, index, { length }) => {
+			// TODO: 참가 여부에 따라서 다르게 표시
+			const isAccepted = index % 2 === 0;
+
+			return (
+				<MyStudyCard
+					key={item.id}
+					studyId={item.id}
+					title={item.title}
+					views={item.views}
+					createdAt={item.createdAt}
+					bookmarks={item.bookmarkCount}
+					className={classNames({ 'mb-438rem': index !== length - 1 })}
+					leftTopComponent={
+						<Box className="applied-studies-chip-con">
+							<Box className="applied-studies-chip">D-18</Box>
+							<Box className={`applied-studies-chip-${isAccepted ? 'filled' : 'disabled'} ml_25rem`}>
+								{isAccepted ? '참가 완료' : '수락 대기중'}
+							</Box>
+						</Box>
+					}
+				/>
+			);
+		});
+	};
+
 	return (
 		<div className="applied-studies-container">
 			<div className="applied-studies-title mt40 mb16">신청중 ({openedAppliedStudies?.length})</div>
 			{openedAppliedStudies?.length === 0 ? (
 				<EmptyComponent title="신청중인 스터디가 없습니다" buttonText="스터디 찾아보기" onClick={findStudies} />
 			) : (
-				<div />
+				renderOpenedAppliedStudies()
 			)}
 			<div className="applied-studies-accordian-container">
 				<div className="applied-studies-accordian-text">마감된 항목</div>

dabc5d1b63b97330ce9517382addbf302678b81d
Jeyoung-Park <jason9380@naver.com>
2022-03-25 21:07:55 +0900

    fix: notice error 수정

--- src/app/profile/my/notice/NoticeContainer.tsx
+++ src/app/profile/my/notice/NoticeContainer.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import useFetchNotices from '@src/hooks/remotes/notice/useFetchNotices';
 import Loader from '@src/components/common/loader/Loader';
 import NoticePresenter from './NoticePresenter';
 
 const NoticeContainer = (): JSX.Element => {
-	const data = useFetchNotices();
+	const { data, isLoading } = useFetchNotices();
 
-	if (data.isLoading) return <Loader />;
-	return data.data ? <NoticePresenter notices={data.data} /> : <div />;
+	if (isLoading) return <Loader />;
+	return data?.data ? <NoticePresenter notices={data.data} /> : <div />;
 };
 
 export default NoticeContainer;

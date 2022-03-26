d04286b2897a383ce206d105f7e4912d3fc65ec7
tngur1101 <tngur1101@naver.com>
2022-03-26 19:59:21 +0900

    inputhandle & css

--- src/app/login/boxContainer/LoginBoxContainer.tsx
+++ src/app/login/boxContainer/LoginBoxContainer.tsx
@@ -6,13 +6,25 @@ const boxcontainer = (): JSX.Element => {
 	const [inputId, setInputId] = useState(null);
 	const [inputPw, setInputPw] = useState(null);
 
-	// const handleInputId = (event:React.ChangeEvent<HTMLInputElement>) => {
-	// 	setInputId(event.target.value);
-	// }
+	const handleInputId = (event:React.ChangeEvent<HTMLInputElement>) => {
+		const {
+			currentTarget: {value},
+		} = event;
+		setInputId(inputId)
+	}
 
-	// const handleInputPw = (event:React.ChangeEvent<HTMLInputElement>) => {
-	// 	setInputPw(event.target.value);
-	// }
+	const handleInputPw = (event:React.ChangeEvent<HTMLInputElement>) => {
+		const {
+			currentTarget: {value},
+		} = event;
+		setInputPw(inputPw)
+	}
+
+	const onSubmit=(event: React.FormEvent<HTMLFormElement>)=>{
+		event.preventDefault();
+		console.log(inputId);
+		console.log(inputPw);
+	}
 
 	const onClickLoginBtn = () => {
 		console.log('clicked!');
@@ -21,15 +33,15 @@ const boxcontainer = (): JSX.Element => {
 	return(
         <div>
             <div className="portal">
-                <form className="input-form">
+                <form className="input-form" onSubmit={onSubmit}>
 					{/* 밑의 value는 input안에 들어가야하는데 오류나서 빼놨습니다 */}
 					{/* value={inputId} onChange={handleInputId} */}
-                    <input type="email" className="portal-id" placeholder="포탈 아이디"/>
-                    <input type="password" className="portal-pw" placeholder="비밀번호"/>
+                    <input type="email" className="portal-id" placeholder="포탈 아이디" onChange={handleInputId} required pattern=".+@globex\.com"/>
+                    <input type="password" className="portal-pw" placeholder="비밀번호" onChange={handleInputPw} required minLength={8}/>
 					{/* value={inputPw} onChange={handleInputPw} */}
 					{/* 얘도 password 안에 들어가야합니다 */}
-                </form>
-                <button type="button" onClick={onClickLoginBtn} className="login-btn"><span className="logbtn">로그인</span></button>
+					<button type="button" onClick={onClickLoginBtn} className="login-btn"><span className="logbtn">로그인</span></button>
+				</form>
 			</div>
 		</div>
 	);

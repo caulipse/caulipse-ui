38be78623218329863fb6cdad9a57f2399a7a8c3
tngur1101 <39721753+tngur1101@users.noreply.github.com>
2022-03-26 21:46:31 +0900

    Add files via upload

---
+++ src/app/login/loginLogo/LoginLogo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './LoginLogo.scss';
+
+const loginlogo = (): JSX.Element => {
+    return(
+        <div>
+            <span className="Login-Logo">
+                로그인
+            </span>
+        </div>
+    );
+};
+
+export default loginlogo;

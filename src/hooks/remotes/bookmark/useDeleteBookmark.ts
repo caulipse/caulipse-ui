342fa46e7ed241dde261e46958151893186fd87d
Jeyoung-Park <jason9380@naver.com>
2022-03-27 21:29:55 +0900

    fix: 북마크 삭제 이후 refetch

--- src/hooks/remotes/bookmark/useDeleteBookmark.ts
+++ src/hooks/remotes/bookmark/useDeleteBookmark.ts
@@ -1,9 +1,11 @@
 import API from '@src/api';
 import useSnackbar from '@src/hooks/snackbar/useSnackbar';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
+import QUERY_KEY from '@src/hooks/remotes';
 
 export default (id: string) => {
 	const { openSnackbar } = useSnackbar();
+	const client = useQueryClient();
 
 	const mutation = async () => {
 		const res = await API.deleteBookmark(id);
@@ -13,6 +15,7 @@ export default (id: string) => {
 	return useMutation(mutation, {
 		onSuccess: (response: any) => {
 			console.log(response);
+			client.refetchQueries(QUERY_KEY.FETCH_BOOKMARKS);
 			openSnackbar('북마크 목록이 삭제되었습니다.');
 		},
 		onError: (e: Error) => {

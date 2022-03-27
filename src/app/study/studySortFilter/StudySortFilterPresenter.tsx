b488b5e79c1865cf4478b3cc17a992534f95ba93
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 <mirijo02233092@gmail.com>
2022-03-27 21:16:19 +0900

    feat: 스터디 리스트 > 필터 활성화 상태 구현

--- src/app/study/studySortFilter/StudySortFilterPresenter.tsx
+++ src/app/study/studySortFilter/StudySortFilterPresenter.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import { IoOptions, IoChevronDown } from 'react-icons/io5';
+import classNames from 'classnames';
 import './index.scss';
 
 interface IStudySortFilterPresenterProps {
 	onClickFilter: () => void;
 	onClickSort: () => void;
 	sort: string;
+	filterNum: number;
 }
 
 const StudySortFilterPresenter = ({
 	onClickFilter,
 	sort,
 	onClickSort,
+	filterNum,
 }: IStudySortFilterPresenterProps): JSX.Element => {
 	return (
 		<Container className="study-sort-filter-presenter-container">
@@ -20,8 +23,12 @@ const StudySortFilterPresenter = ({
 				<span>{sort}</span>
 				<IoChevronDown size={16} />
 			</Container>
-			<Container className="study-sort-filter-presenter-container-filter" onClick={onClickFilter}>
+			<Container
+				className={classNames('study-sort-filter-presenter-container-filter', { enabled: !!filterNum })}
+				onClick={onClickFilter}
+			>
 				<IoOptions size={16} />
+				{!!filterNum && <span className="study-sort-filter-presenter-filter-chip">{filterNum}</span>}
 				<span>필터</span>
 			</Container>
 		</Container>

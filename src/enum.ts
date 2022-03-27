d189c505b2044d56afa96132330fe69ec3af0da8
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 <mirijo02233092@gmail.com>
2022-03-27 12:40:37 +0900

    refactor: 스터디 정렬 옵션 enum 리팩토링

--- src/enum.ts
+++ src/enum.ts
@@ -6,4 +6,9 @@ export enum REPORT_TYPE_ENUM {
 	REPORT_TYPE_ENUM_4 = '기타',
 }
 
-export enum STUDY_SORT_ENUM {}
+export enum STUDY_SORT_OPTION_ENUM {
+	CREATED_AT_DESC = '최근 등록순',
+	CREATED_AT_ASC = '오래된 순',
+	VACANCY_DESC = '남은 모집인원: 많은 순',
+	VACANCY_ASC = '남은 모집인원: 적은 순',
+}

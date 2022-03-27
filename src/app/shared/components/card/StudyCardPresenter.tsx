ae117063ac8019738b86464f722061d1942d4d3d
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 <mirijo02233092@gmail.com>
2022-03-27 20:28:23 +0900

    fix: 스터디 카드 내 디데이 칩 위치 웹/모바일 분기 처리

--- src/app/shared/components/card/StudyCardPresenter.tsx
+++ src/app/shared/components/card/StudyCardPresenter.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Container, Typography } from '@material-ui/core';
 import { Study } from '@api/types';
 import ProgressBar from '@common/progress/ProgressBar';
 import { getMainCategoryLabel, getSubCategoryLabel } from '@shared/utils/category';
+import useWindowDimensions from '@src/hooks/useWindowDimensions';
+
 import './index.scss';
 
 interface PresenterProps {
 	study: Study;
 }
-const StudyCardPresenter = ({ study }: PresenterProps): JSX.Element => (
-	<Container className="study-card-container">
-		<Container className="study-card-category-container">
-			<span className="study-card-category">
-				{getMainCategoryLabel(Number(study.categoryCode))} {'>'} {getSubCategoryLabel(Number(study.categoryCode))}
-			</span>
-			<Container className="study-card-date">D-16</Container>
+const StudyCardPresenter = ({ study }: PresenterProps): JSX.Element => {
+	const { width } = useWindowDimensions();
+	const isDesktop = useMemo(() => {
+		return width > 1024;
+	}, [width]);
+	return (
+		<Container className="study-card-container">
+			<Container className="study-card-category-container">
+				{isDesktop && <Container className="study-card-date">D-16</Container>}
+				<span className="study-card-category">
+					{getMainCategoryLabel(Number(study.categoryCode))} {'>'} {getSubCategoryLabel(Number(study.categoryCode))}
+				</span>
+				{!isDesktop && <Container className="study-card-date">D-16</Container>}
+			</Container>
+			<Typography className="study-card-title">{study.title}</Typography>
+			<Typography className="study-card-tag">
+				#{study.frequency} #{study.weekday} #{study.location}
+			</Typography>
+			<ProgressBar max={study.capacity} current={study.membersCount} />
 		</Container>
-		<Typography className="study-card-title">{study.title}</Typography>
-		<Typography className="study-card-tag">
-			#{study.frequency} #{study.weekday} #{study.location}
-		</Typography>
-		<ProgressBar max={study.capacity} current={study.membersCount} />
-	</Container>
-);
+	);
+};
 
 export default StudyCardPresenter;

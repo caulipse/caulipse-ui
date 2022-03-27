1d6619d575995c77dde7a81e0028ef4e19e6ba1b
Jeyoung-Park <jason9380@naver.com>
2022-03-27 21:31:28 +0900

    fix: post comment 이후 refetch

--- src/hooks/remotes/comment/usePostStudyComment.ts
+++ src/hooks/remotes/comment/usePostStudyComment.ts
@@ -1,10 +1,12 @@
 import API from '@src/api';
 import { IRequestPostStudyComment } from '@src/api/request/studyComment';
 import useSnackbar from '@src/hooks/snackbar/useSnackbar';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
+import QUERY_KEY from '..';
 
 export default () => {
 	const { openSnackbar } = useSnackbar();
+	const client = useQueryClient();
 
 	const mutation = async (request: IRequestPostStudyComment) => {
 		const res = await API.postStudyComment(request);
@@ -14,6 +16,7 @@ export default () => {
 	return useMutation(mutation, {
 		onSuccess: (response: any) => {
 			openSnackbar('댓글 작성에 성공하였습니다.');
+			client.refetchQueries(QUERY_KEY.FETCH_STUDY_COMMENTS);
 			console.log(response);
 		},
 		onError: (e: Error) => {

74232856f5180a56a59f347af26f311fb3a348ef
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 <mirijo02233092@gmail.com>
2022-03-27 12:36:52 +0900

    refactor: 신고 사유 enum 으로 관리하도록 리팩토링

--- src/app/modal/ReportModal.tsx
+++ src/app/modal/ReportModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Container, Typography, Radio, RadioGroup, FormControlLabel } from '@material-ui/core';
+import { Button, Container, Radio, RadioGroup, FormControlLabel } from '@material-ui/core';
 import '@common/modal/common.scss';
 import SimpleModal from '@common/modal/SimpleModal';
 import { IModalContainerCommonProps } from '@common/modal/types';
@@ -7,6 +7,7 @@ import { useAtom } from 'jotai';
 import globalState from '@src/state';
 import useFetchStudy from '@src/hooks/remotes/study/useFetchStudy';
 import config from '@src/config';
+import { REPORT_TYPE_ENUM } from '@src/enum';
 
 const ReportModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element => {
 	const [state] = useAtom(globalState);
@@ -15,13 +16,21 @@ const ReportModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element
 	const { data } = useFetchStudy(modal?.params);
 	const { study } = data!;
 
-	const [value, setValue] = useState('욕설 및 비방');
+	const {
+		REPORT_TYPE_ENUM_0,
+		REPORT_TYPE_ENUM_1,
+		REPORT_TYPE_ENUM_2,
+		REPORT_TYPE_ENUM_3,
+		REPORT_TYPE_ENUM_4,
+	} = REPORT_TYPE_ENUM;
+
+	const [value, setValue] = useState(REPORT_TYPE_ENUM_0);
 	const onClick = () => {
 		// TODO
 		// 신고 API 연동
 	};
 	const onChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-		setValue((evt.target as HTMLInputElement).value);
+		setValue((evt.target as HTMLInputElement).value as REPORT_TYPE_ENUM);
 	};
 
 	const content = `mailto:${config.mail}?subject=[신고] ${value}&body=* [${study.title}]%0D%0A* 신고자_${userId}%0D%0A* 신고대상_${study.HOST_ID}`;
@@ -31,39 +40,39 @@ const ReportModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element
 			<Container className="simple-modal-content-container">
 				<RadioGroup value={value} onChange={onChange}>
 					<FormControlLabel
-						className={value === '욕설 및 비방' ? 'radio-container selected' : 'radio-container'}
+						className={value === REPORT_TYPE_ENUM_0 ? 'radio-container selected' : 'radio-container'}
 						labelPlacement="start"
-						value="욕설 및 비방"
+						value={REPORT_TYPE_ENUM_0}
 						control={<Radio />}
-						label="욕설 및 비방"
+						label={REPORT_TYPE_ENUM_0}
 					/>
 					<FormControlLabel
-						className={value === '사칭, 사기' ? 'radio-container selected' : 'radio-container'}
+						className={value === REPORT_TYPE_ENUM_1 ? 'radio-container selected' : 'radio-container'}
 						labelPlacement="start"
-						value="사칭, 사기"
+						value={REPORT_TYPE_ENUM_1}
 						control={<Radio />}
-						label="사칭, 사기"
+						label={REPORT_TYPE_ENUM_1}
 					/>
 					<FormControlLabel
-						className={value === '상업적 광고' ? 'radio-container selected' : 'radio-container'}
+						className={value === REPORT_TYPE_ENUM_2 ? 'radio-container selected' : 'radio-container'}
 						labelPlacement="start"
-						value="상업적 광고"
+						value={REPORT_TYPE_ENUM_2}
 						control={<Radio />}
-						label="상업적 광고"
+						label={REPORT_TYPE_ENUM_2}
 					/>
 					<FormControlLabel
-						className={value === '음란물, 불건전한 게시물' ? 'radio-container selected' : 'radio-container'}
+						className={value === REPORT_TYPE_ENUM_3 ? 'radio-container selected' : 'radio-container'}
 						labelPlacement="start"
-						value="음란물, 불건전한 게시물"
+						value={REPORT_TYPE_ENUM_3}
 						control={<Radio />}
-						label="음란물, 불건전한 게시물"
+						label={REPORT_TYPE_ENUM_3}
 					/>
 					<FormControlLabel
-						className={value === '기타' ? 'radio-container selected' : 'radio-container'}
+						className={value === REPORT_TYPE_ENUM_4 ? 'radio-container selected' : 'radio-container'}
 						labelPlacement="start"
-						value="기타"
+						value={REPORT_TYPE_ENUM_4}
 						control={<Radio />}
-						label="기타"
+						label={REPORT_TYPE_ENUM_4}
 					/>
 				</RadioGroup>
 				<Container style={{ padding: 0, marginTop: '2rem', display: 'flex' }}>
---
+++ src/enum.ts
@@ -0,0 +1,9 @@
+export enum REPORT_TYPE_ENUM {
+	REPORT_TYPE_ENUM_0 = '욕설 및 비방',
+	REPORT_TYPE_ENUM_1 = '사칭, 사기',
+	REPORT_TYPE_ENUM_2 = '상업적 광고',
+	REPORT_TYPE_ENUM_3 = '음란물, 불건전한 게시물',
+	REPORT_TYPE_ENUM_4 = '기타',
+}
+
+export enum STUDY_SORT_ENUM {}

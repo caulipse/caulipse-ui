ec41e8e34de47cb77efb15b215d34c1f43ce1576
tngur1101 <39721753+tngur1101@users.noreply.github.com>
2022-03-31 01:24:18 +0900

    Update index.tsx
    login modal을 넣는다고 넣었는데 오류 나는데 어떻게 해야할지 잘 모르겠어요

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,25 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import LoginLogo from "@src/app/login/loginLogo/LoginLogo";
 import BoxContainer from "@src/app/login/boxContainer/LoginBoxContainer";
 import './styles.scss';
 // import CloseButton from "@src/components/common/iconButton/CloseButton";
 import FooterContainer from "@src/app/login/footerContainer/LoginFooterContainer";
+import LModal from "./modalComponents";
+import useModal from '@src/hooks/modal/useModal';
+import { IModalContainerCommonProps } from '@common/modal/types';
 
-const LoginPage = (): JSX.Element => {
+const LoginPage = ({ open, onClose }: IModalContainerCommonProps): JSX.Element => {
+    // const [modalOpen, setModalOpen] = useState(false);
+
+    // const toggleModal = () => setModalOpen(!modalOpen);
+    const onClick = useCallback(() => {
+		onClose(false);
+		const { openModal } = useModal();
+	}, []);
 
     return(
-        // CloseButton onClick사용법 미리님께 질문
-        <div className="Login-default">
-            {/* <CloseButton />  */}
-        <div className="login-logo">
-            <LoginLogo />
-            </div>
-                <div className="login-box">
-                    <BoxContainer />
-                    <FooterContainer /> 
-                </div>
-        </div>
+        // <div className="Login-default">
+        // <div className="login-logo">
+        //     <LoginLogo />
+        //     </div>
+        //         <div className="login-box">
+        //             <BoxContainer />
+        //             <FooterContainer /> 
+        //         </div>
+        // </div>
 
+        <LModal 
+        open={open} onClick={onClose} height="48.75rem" //이부분에서 에러나는데 잘 모르겠습니다
+        >
+        <div className="Login-default">
+            <div className="login-logo">
+                <LoginLogo />
+            </div>
+            <div className="login-box">
+                <BoxContainer />
+                <FooterContainer /> 
+            </div>
+        </div>
+        </LModal>
     );
 };
 

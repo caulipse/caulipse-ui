88c460d13f270e7bea14633745544cb369c2e1fb
Jeyoung-Park <jason9380@naver.com>
2022-03-30 23:25:58 +0900

    fix: 모집글 수정 모달 헤더

--- src/app/modal/EditStudyModal.tsx
+++ src/app/modal/EditStudyModal.tsx
@@ -1,21 +1,52 @@
-import { Box } from '@material-ui/core';
+import { Box, Button } from '@material-ui/core';
 import Modal from '@src/components/common/modal/Modal';
 import { IModalContainerCommonProps } from '@src/components/common/modal/types';
-import React from 'react';
+import classNames from 'classnames';
+import React, { useCallback, useState } from 'react';
 import { IoClose } from 'react-icons/io5';
 import './editStudyModal.scss';
 
+const EDIT_STUDY_TAB_ENUM = {
+	TAG: '태그 수정',
+	CONTENT: '글 수정',
+};
+
 const EditStudyModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element => {
-	return (
-		<Modal open={open} onClose={onClose} isFullHeight>
+	const [currentTab, setCurrentTab] = useState(EDIT_STUDY_TAB_ENUM.TAG);
+
+	const renderHeader = useCallback(() => {
+		return (
 			<>
 				<Box style={{ backgroundColor: 'green' }} className="edit-study-modal-header-con">
 					<IoClose className="edit-study-modal-header-close-icn" color="#ffffff" onClick={() => onClose(false)} />
 					<Box className="edit-study-modal-header-title">모집글 수정하기</Box>
 					<Box className="edit-study-modal-header-close-icn" />
 				</Box>
-				<Box>태그 수정</Box>
+				<Box className="edit-study-modal-tab-con">
+					<Button
+						className={classNames('edit-study-modal-tab-btn', {
+							'edit-study-modal-tab-btn-selected': currentTab === EDIT_STUDY_TAB_ENUM.TAG,
+						})}
+						onClick={() => setCurrentTab(EDIT_STUDY_TAB_ENUM.TAG)}
+					>
+						태그 수정
+					</Button>
+					<Button
+						className={classNames('edit-study-modal-tab-btn', {
+							'edit-study-modal-tab-btn-selected': currentTab === EDIT_STUDY_TAB_ENUM.CONTENT,
+						})}
+						onClick={() => setCurrentTab(EDIT_STUDY_TAB_ENUM.CONTENT)}
+					>
+						글 수정
+					</Button>
+				</Box>
 			</>
+		);
+	}, [currentTab]);
+
+	return (
+		<Modal open={open} onClose={onClose} isFullHeight>
+			<>{renderHeader()}</>
 		</Modal>
 	);
 };

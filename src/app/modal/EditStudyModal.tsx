d9e1b7ab98bb5a9dfb548239ef77a3b04f6d069f
Jeyoung-Park <jason9380@naver.com>
2022-03-30 23:07:42 +0900

    feat: header

--- src/app/modal/EditStudyModal.tsx
+++ src/app/modal/EditStudyModal.tsx
@@ -1,11 +1,21 @@
+import { Box } from '@material-ui/core';
 import Modal from '@src/components/common/modal/Modal';
 import { IModalContainerCommonProps } from '@src/components/common/modal/types';
 import React from 'react';
+import { IoClose } from 'react-icons/io5';
+import './editStudyModal.scss';
 
 const EditStudyModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element => {
 	return (
 		<Modal open={open} onClose={onClose} isFullHeight>
-			<div style={{ backgroundColor: 'red' }}>모집글 수정하기</div>
+			<>
+				<Box style={{ backgroundColor: 'green' }} className="edit-study-modal-header-con">
+					<IoClose className="edit-study-modal-header-close-icn" color="#ffffff" />
+					<Box className="edit-study-modal-header-title">모집글 수정하기</Box>
+					<Box className="edit-study-modal-header-close-icn" />
+				</Box>
+				<Box>태그 수정</Box>
+			</>
 		</Modal>
 	);
 };

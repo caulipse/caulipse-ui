1be47fbcd9e1c4ab8f61332e329e86572db8f089
Jeyoung-Park <jason9380@naver.com>
2022-04-01 01:59:08 +0900

    fix: useFetchPArticipants 적용

--- src/app/studyDetail/studyContent/currentState/StudyCurrentStateContainer.tsx
+++ src/app/studyDetail/studyContent/currentState/StudyCurrentStateContainer.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import useFetchStudyUsers from '@src/hooks/remotes/studyUser/useFetchStudyUsers';
 import Loader from '@src/components/common/loader/Loader';
 import useFetchUserProfile from '@src/hooks/remotes/user/useFetchUserProfile';
+import useFetchStudyParticipants from '@src/hooks/remotes/studyUser/useFetchStudyParticipants';
 import StudyCurrentStatePresenter from './StudyCurrentStatePresenter';
 
 interface StudyCurrentStateContainerProps {
@@ -17,14 +18,19 @@ const StudyCurrentStateContainer = ({
 	capacity,
 	isHost,
 }: StudyCurrentStateContainerProps): JSX.Element => {
-	const { data, isLoading } = useFetchStudyUsers(studyId);
+	const { data: studyParticipantsData, isLoading } = useFetchStudyParticipants(studyId);
 	const { data: hostData, isLoading: isHostLoading } = useFetchUserProfile(hostId);
 
 	if (isLoading || isHostLoading) return <Loader />;
 
 	// TODO: 수락 대기중 api 추가
-	return data && hostData ? (
-		<StudyCurrentStatePresenter host={hostData.userProfile} studyUsers={data} capacity={capacity} isHost={isHost} />
+	return studyParticipantsData && hostData ? (
+		<StudyCurrentStatePresenter
+			host={hostData.userProfile}
+			studyUsers={studyParticipantsData}
+			capacity={capacity}
+			isHost={isHost}
+		/>
 	) : (
 		<div />
 	);

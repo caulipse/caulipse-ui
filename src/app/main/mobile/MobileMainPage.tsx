e590b778e053510c91ee481040a4ae3843222a0a
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 <mirijo02233092@gmail.com>
2022-04-02 14:11:27 +0900

    feat: 메인 페이지(mobil)에 무한스크롤 적용

--- src/app/main/mobile/MobileMainPage.tsx
+++ src/app/main/mobile/MobileMainPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useState, useRef, useEffect, RefObject } from 'react';
 import { MainCategoryType, CategoryType } from '@src/types';
 import { Container, Typography, Grid } from '@material-ui/core';
 import MainCategoryItem from '@src/app/main/MainCategoryItem';
@@ -8,13 +8,45 @@ import StudyCardContainer from '@src/app/shared/components/card/StudyCardContain
 import fetchStudies from '@src/hooks/remotes/study/useFetchStudies';
 import { Study } from '@api/types';
 import { useHistory } from 'react-router-dom';
+import { useAtom } from 'jotai';
+import { studyListState } from '@src/state';
+import useIntersectionObserver from '@src/hooks/common/useIntersectionObserver';
 import MainButton from '../button/MainButton';
 import './index.scss';
 
 const MobileMainPage = (): JSX.Element => {
 	const history = useHistory();
-	const { data, isLoading } = fetchStudies('최근 등록순');
-	const studies = data?.studies || ([] as Study[]);
+
+	const [state, setState] = useAtom(studyListState);
+
+	const { filterOption, paginationOption } = state;
+	const { pageNo } = paginationOption;
+
+	const { data, isLoading } = fetchStudies('최근 등록순', filterOption, paginationOption);
+
+	const totalPage = useMemo(() => {
+		return data?.pages ?? 0;
+	}, [data]);
+
+	const target = useRef<HTMLDivElement>(null);
+	const isOnScreen = useIntersectionObserver(target);
+
+	const [studies, setStudies] = useState([] as Study[]);
+
+	useEffect(() => {
+		if (!data?.studies) return;
+		if (pageNo === 1) {
+			setStudies(data?.studies);
+		} else {
+			setStudies(studies.concat(data?.studies));
+		}
+	}, [data]);
+
+	useEffect(() => {
+		if (isOnScreen && pageNo && pageNo < totalPage) {
+			setState({ ...state, paginationOption: { ...state?.paginationOption, pageNo: pageNo + 1 } });
+		}
+	}, [isOnScreen]);
 
 	const onClick = (category: MainCategoryType) => {
 		if (category.path === '') {
@@ -44,11 +76,10 @@ const MobileMainPage = (): JSX.Element => {
 			</Container>
 			<Container className="mobile-main-page-study-list-container">
 				<Typography>곧 마감이되는 스터디들이에요!</Typography>
-				{isLoading ? (
-					<Loader />
-				) : (
-					studies && studies?.map((study) => <StudyCardContainer study={study} key={study.id} />)
-				)}
+				{studies?.map((study) => (
+					<StudyCardContainer study={study} key={study.id} />
+				))}
+				<Container ref={(target as unknown) as RefObject<HTMLDivElement> | null}>{isLoading && <Loader />}</Container>
 			</Container>
 			<MainButton />
 		</>

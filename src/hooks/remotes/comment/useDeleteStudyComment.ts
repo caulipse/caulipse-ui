d79a0d5eb5f1152fee14c8d720b25881724b25bf
Jeyoung-Park <jason9380@naver.com>
2022-04-03 14:25:36 +0900

    fix: 삭제 이후 refetch 추가

--- src/hooks/remotes/comment/useDeleteStudyComment.ts
+++ src/hooks/remotes/comment/useDeleteStudyComment.ts
@@ -1,9 +1,11 @@
 import API from '@src/api';
 import useSnackbar from '@src/hooks/snackbar/useSnackbar';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
+import QUERY_KEY from '..';
 
 export default (studyId: string, commentId: string) => {
 	const { openSnackbar } = useSnackbar();
+	const client = useQueryClient();
 
 	const mutation = async () => {
 		const res = await API.deleteStudyComment({ id: studyId, commentId });
@@ -13,6 +15,7 @@ export default (studyId: string, commentId: string) => {
 	return useMutation(mutation, {
 		onSuccess: (response: any) => {
 			console.log(response);
+			client.refetchQueries(QUERY_KEY.FETCH_STUDY_COMMENTS);
 			openSnackbar('댓글 삭제되었습니다.');
 		},
 		onError: (e: Error) => {

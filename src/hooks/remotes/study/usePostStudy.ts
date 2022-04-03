72bac60233eac1750a9ff53cb39ba00d5f7242f0
Jeyoung-Park <jason9380@naver.com>
2022-04-03 20:51:57 +0900

    feat: usePOstStudy

---
+++ src/hooks/remotes/study/usePostStudy.ts
@@ -0,0 +1,19 @@
+import { useMutation } from 'react-query';
+import API from '@src/api';
+import { IRequestPostStudy } from '@src/api/request/study';
+
+export default () => {
+	const mutation = async (request: IRequestPostStudy) => {
+		const res = await API.postStudy(request);
+		return res.data;
+	};
+
+	return useMutation(mutation, {
+		onSuccess: (response: any) => {
+			console.log(response);
+		},
+		onError: (e: Error) => {
+			console.error(e.message);
+		},
+	});
+};

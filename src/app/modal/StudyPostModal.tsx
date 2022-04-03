4e19c2210413ccf4316d3f5dded5d4aa7f825fb1
Jeyoung-Park <jason9380@naver.com>
2022-04-03 16:59:07 +0900

    feat: header

--- src/app/modal/StudyPostModal.tsx
+++ src/app/modal/StudyPostModal.tsx
@@ -1,12 +1,20 @@
+import { Box } from '@material-ui/core';
 import Modal from '@src/components/common/modal/Modal';
 import { IModalContainerCommonProps } from '@src/components/common/modal/types';
 import React from 'react';
+import { IoClose } from 'react-icons/io5';
 import './studyPostModal.scss';
 
 const StudyPostModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element => {
 	return (
 		<Modal open={open} onClose={onClose} isFullHeight>
-			<>studymodal</>
+			<>
+				<Box className="study-post-modal-header-con">
+					<IoClose className="study-post-modal-icon" color="#ffffff" onClick={() => onClose(false)} />
+					<Box>Logo</Box>
+					<Box className="study-post-modal-icon" />
+				</Box>
+			</>
 		</Modal>
 	);
 };

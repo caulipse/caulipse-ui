2fdc9ee29ffb7d438ac45b20bcf64c3c29d4d115
Jeyoung-Park <jason9380@naver.com>
2022-04-03 14:55:00 +0900

    feat: footer ui

--- src/app/shared/components/footer/index.tsx
+++ src/app/shared/components/footer/index.tsx
@@ -1,12 +1,28 @@
+import { Box, Button } from '@material-ui/core';
 import React from 'react';
 import './index.scss';
+import config from '@src/config';
 
 const Footer: React.FC = () => (
-  <div className="footer-con">
-    <div className="footer-wrap">
-      footer
-    </div>
-  </div>
-)
+	<Box className="footer-con">
+		<Box className="footer-wrap">
+			<Box className="footer-row">
+				<Button className="footer-title" variant="text">
+					서비스 피드백
+				</Button>
+				<Button className="footer-title" variant="text">
+					About Us
+				</Button>
+				<Button className="footer-title" variant="text">
+					Instagram
+				</Button>
+			</Box>
+			<Box className="footer-title mt_719rem">E-mail: {config.mail}</Box>
+			<Button className="footer-title mt_75rem mb3rem" variant="text">
+				개인정보처리방침
+			</Button>
+		</Box>
+	</Box>
+);
 
 export default Footer;

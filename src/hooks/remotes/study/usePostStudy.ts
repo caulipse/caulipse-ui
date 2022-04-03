e3189f54245bac9d77eb2c5e77160682766a925e
Jeyoung-Park <jason9380@naver.com>
2022-04-03 20:59:05 +0900

    feat: 스터디 생성 이후 refetch 추가

--- src/hooks/remotes/study/usePostStudy.ts
+++ src/hooks/remotes/study/usePostStudy.ts
@@ -1,8 +1,11 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import API from '@src/api';
 import { IRequestPostStudy } from '@src/api/request/study';
+import QUERY_KEY from '..';
 
 export default () => {
+	const client = useQueryClient();
+
 	const mutation = async (request: IRequestPostStudy) => {
 		const res = await API.postStudy(request);
 		return res.data;
@@ -11,6 +14,7 @@ export default () => {
 	return useMutation(mutation, {
 		onSuccess: (response: any) => {
 			console.log(response);
+			client.refetchQueries(QUERY_KEY.FETCH_STUDIES);
 		},
 		onError: (e: Error) => {
 			console.error(e.message);

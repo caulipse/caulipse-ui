73f8ce89960707e5d7e2790e44dadf6cd7adb78d
Jeyoung-Park <jason9380@naver.com>
2022-04-04 22:39:41 +0900

    feat: 회원가입 암호찾기

--- src/app/modal/LoginModal.tsx
+++ src/app/modal/LoginModal.tsx
@@ -1,4 +1,6 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, Button, Typography } from '@material-ui/core';
+import CommonButton from '@src/components/common/button/CommonButton';
+import { ButtonTypeEnum } from '@src/components/common/button/types';
 import Modal from '@src/components/common/modal/Modal';
 import { IModalContainerCommonProps } from '@src/components/common/modal/types';
 import CommonTextField from '@src/components/common/textfield/CommonTextField';
@@ -9,6 +11,10 @@ import './loginModal.scss';
 const LoginModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element => {
 	const [email, setEmail] = useState<string>('');
 	const [password, setPassword] = useState<string>('');
+
+	const handleLogin = () => {
+		// TODO: handleLogin
+	};
 
 	return (
 		<Modal open={open} onClose={onClose} isFullHeight>
@@ -31,6 +37,16 @@ const LoginModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element 
 						onChange={(e) => setPassword(e.target.value)}
 						textFieldProps={{ type: 'password' }}
 					/>
+					<CommonButton className="mt1_5rem" type={ButtonTypeEnum.primary} title="로그인" onClick={handleLogin} />
+					<Box className="login-modal-body-btns-con">
+						<Button className="login-modal-body-text-btn" variant="text">
+							회원가입
+						</Button>
+						<Box className="login-modal-body-vertical-divider" />
+						<Button className="login-modal-body-text-btn" variant="text">
+							암호찾기
+						</Button>
+					</Box>
 				</Box>
 			</Box>
 		</Modal>

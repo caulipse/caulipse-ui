3a20b60ca052d455cc77f7642fa9e189cef996ab
Jeyoung-Park <jason9380@naver.com>
2022-04-10 00:47:28 +0900

    feat: 헤더

--- src/pages/signup/ResetPwPage.tsx
+++ src/pages/signup/ResetPwPage.tsx
@@ -1,7 +1,18 @@
+import { Box, Typography } from '@material-ui/core';
 import React from 'react';
+import { IoArrowBack, IoClose } from 'react-icons/io5';
+import './index.scss';
 
-function ResetPwPage() {
-	return <div>ResetPwPage</div>;
-}
+const ResetPwPage = (): JSX.Element => {
+	return (
+		<Box>
+			<Box className="reset-pw-header-con">
+				<IoArrowBack className="reset-pw-icon" color="#fff" />
+				<Typography>로고</Typography>
+				<Box className="reset-pw-icon" />
+			</Box>
+		</Box>
+	);
+};
 
 export default ResetPwPage;

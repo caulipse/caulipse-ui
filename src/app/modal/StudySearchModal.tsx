c5cb410ae7f6dd9dba01edcb8debfe9305b3c806
Jeyoung-Park <jason9380@naver.com>
2022-04-10 22:52:45 +0900

    fix: studySearchheader

--- src/app/modal/StudySearchModal.tsx
+++ src/app/modal/StudySearchModal.tsx
@@ -1,13 +1,18 @@
 import { Box } from '@material-ui/core';
 import { IModalContainerCommonProps } from '@src/components/common/modal/types';
 import React from 'react';
+import { IoArrowBack, IoClose } from 'react-icons/io5';
 import './studySearchModal.scss';
 
 const StudySearchModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element | false => {
 	return (
 		open && (
 			<Box className="study-search-modal-overlay" onClick={() => onClose(false)}>
-				<div>studysearch</div>
+				<Box className="study-search-modal-header-con">
+					<IoClose className="study-search-modal-icon" color="#fff" onClick={() => onClose(false)} />
+					<Box className="study-search-modal-header-title">스터디 검색</Box>
+					<Box className="study-search-modal-icon" />
+				</Box>
 			</Box>
 		)
 	);

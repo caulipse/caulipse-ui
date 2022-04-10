057b6084fbfdfb5c646d96ee03d970f987fba931
Jeyoung-Park <jason9380@naver.com>
2022-04-10 23:07:16 +0900

    feat: textfield 추가

--- src/app/modal/StudySearchModal.tsx
+++ src/app/modal/StudySearchModal.tsx
@@ -1,10 +1,12 @@
-import { Box } from '@material-ui/core';
+import { Box, TextField } from '@material-ui/core';
 import { IModalContainerCommonProps } from '@src/components/common/modal/types';
-import React from 'react';
+import React, { useState } from 'react';
 import { IoArrowBack, IoClose } from 'react-icons/io5';
 import './studySearchModal.scss';
 
 const StudySearchModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element | false => {
+	const [searchText, setSearchTest] = useState<string>('');
+
 	return (
 		open && (
 			<Box className="study-search-modal-overlay" onClick={() => onClose(false)}>
@@ -12,6 +14,16 @@ const StudySearchModal = ({ open, onClose }: IModalContainerCommonProps): JSX.El
 					<IoClose className="study-search-modal-icon" color="#fff" onClick={() => onClose(false)} />
 					<Box className="study-search-modal-header-title">스터디 검색</Box>
 					<Box className="study-search-modal-icon" />
+				</Box>
+				<Box className="study-search-modal-input-con">
+					<TextField
+						className="study-search-modal-input"
+						placeholder="어떤 스터디를 찾고 계신가요?"
+						value={searchText}
+						onChange={(e) => setSearchTest(e.target.value)}
+						onClick={(e) => e.stopPropagation()}
+						fullWidth
+					/>
 				</Box>
 			</Box>
 		)

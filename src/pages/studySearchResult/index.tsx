972b82e0e988924861f92e591537302438d9b12f
Jeyoung-Park <jason9380@naver.com>
2022-04-12 00:09:20 +0900

    feat: searchKeyword 파라미터로 넘겨주기

--- src/pages/studySearchResult/index.tsx
+++ src/pages/studySearchResult/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+interface StudySearchResultPageProps {
+	searchKeyword?: string;
+}
 
 const StudySearchResultPage = (): JSX.Element => {
+	const location = useLocation<StudySearchResultPageProps>();
+	const searchKeyword = location.state?.searchKeyword;
+
+	console.log('searchkeyword, ', searchKeyword);
+
 	return <div>StudySearchResultPage</div>;
 };
 

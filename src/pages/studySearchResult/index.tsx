607fc5051b83e4035cda8c01743884f9ad1586fc
Jeyoung-Park <jason9380@naver.com>
2022-04-12 00:29:37 +0900

    feat: 입력한 검색어 헤더

--- src/pages/studySearchResult/index.tsx
+++ src/pages/studySearchResult/index.tsx
@@ -1,5 +1,8 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { IoClose } from 'react-icons/io5';
+import { useLocation, useHistory } from 'react-router-dom';
+import './index.scss';
 
 interface StudySearchResultPageProps {
 	searchKeyword?: string;
@@ -7,11 +10,19 @@ interface StudySearchResultPageProps {
 
 const StudySearchResultPage = (): JSX.Element => {
 	const location = useLocation<StudySearchResultPageProps>();
+	const history = useHistory();
 	const searchKeyword = location.state?.searchKeyword;
 
-	console.log('searchkeyword, ', searchKeyword);
-
-	return <div>StudySearchResultPage</div>;
+	return (
+		<>
+			<Box className="search-result-header-con">
+				<Box className="search-result-header-text-con">
+					<Box className="search-result-header-text">{searchKeyword}</Box>
+					<IoClose className="search-result-icon" color="#4c4c4c" onClick={() => history.goBack()} />
+				</Box>
+			</Box>
+		</>
+	);
 };
 
 export default StudySearchResultPage;

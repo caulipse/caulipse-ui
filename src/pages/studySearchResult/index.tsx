7e1424216ea7b43e659c80fac1602cda0be973aa
Jeyoung-Park <jason9380@naver.com>
2022-04-13 00:53:16 +0900

    fix: 스터디 검색 헤더 누르면 검색 모달

--- src/pages/studySearchResult/index.tsx
+++ src/pages/studySearchResult/index.tsx
@@ -3,6 +3,8 @@ import StudyCardContainer from '@src/app/shared/components/card/StudyCardContain
 import EmptyComponent from '@src/app/shared/components/emptyComponents';
 import StudySortFilterContainer from '@src/app/study/studySortFilter/StudySortFilterContainer';
 import Loader from '@src/components/common/loader/Loader';
+import ModalKeyEnum from '@src/components/common/modal/enum';
+import useModal from '@src/hooks/modal/useModal';
 import useFetchSearchResult from '@src/hooks/remotes/study/useFetchSearchResult';
 import React from 'react';
 import { IoArrowForward, IoChevronForward, IoClose } from 'react-icons/io5';
@@ -17,14 +19,23 @@ const StudySearchResultPage = (): JSX.Element => {
 	const location = useLocation<StudySearchResultPageProps>();
 	const history = useHistory();
 	const searchKeyword = location.state?.searchKeyword;
+	const { openModal } = useModal();
 	const { data, isLoading } = useFetchSearchResult(searchKeyword ?? '');
+
+	const goToSearch = () => {
+		openModal(ModalKeyEnum.StudySearchModal, {
+			onSearch: (searchText: string) => {
+				history.push({ pathname: '/study/search', state: { searchKeyword: searchText } });
+			},
+		});
+	};
 
 	if (isLoading) return <Loader />;
 
 	return (
 		<>
 			<Box className="search-result-header-con">
-				<Box className="search-result-header-text-con">
+				<Box className="search-result-header-text-con" onClick={goToSearch}>
 					<Box className="search-result-header-text">{searchKeyword}</Box>
 					<IoClose className="search-result-icon" color="#4c4c4c" onClick={() => history.goBack()} />
 				</Box>

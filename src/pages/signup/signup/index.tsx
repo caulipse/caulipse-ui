08f3a60966988d23c91d3d29ead5c5233f8f4576
Jeyoung-Park <jason9380@naver.com>
2022-04-16 01:22:06 +0900

    feat: 회원가입 헤더

--- src/pages/signup/signup/index.tsx
+++ src/pages/signup/signup/index.tsx
@@ -1,7 +1,21 @@
+import { Box, Typography } from '@material-ui/core';
 import React from 'react';
+import { IoArrowBack } from 'react-icons/io5';
+import { useHistory } from 'react-router-dom';
+import './index.scss';
 
 const SignUpPage = (): JSX.Element => {
-	return <div>SignUpPage</div>;
+	const history = useHistory();
+
+	return (
+		<Box className="sign-up-con">
+			<Box className="sign-up-header-con">
+				<IoArrowBack className="sign-up-icon" color="#ffffff" onClick={() => history.goBack()} />
+				<Typography className="sign-up-header-title">회원가입</Typography>
+				<Box className="sign-up-icon" />
+			</Box>
+		</Box>
+	);
 };
 
 export default SignUpPage;

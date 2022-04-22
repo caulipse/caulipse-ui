d41d0ff98304513397fa938faa47d90511be7108
Jeyoung-Park <jason9380@naver.com>
2022-04-22 20:45:17 +0900

    fix: mapper 추가

---
+++ src/app/shared/utils/studyMapper.ts
@@ -0,0 +1,26 @@
+export const frequencyMapper = {
+	once: '1회',
+	twice: '주 2~4회',
+	more: '주 5회 이상',
+};
+
+export const weekdayMapper = {
+	mon: '월',
+	tue: '화',
+	wed: '수',
+	thu: '목',
+	fri: '금',
+	sat: '토',
+	sun: '일',
+};
+
+export const locationMapper = {
+	no_contact: '비대면',
+	room: '학교 스터디룸',
+	library: '중앙 도서관',
+	study_cafe: '스터디 카페',
+	cafe: '카페',
+	loc1: '흑석, 상도',
+	loc2: '서울대입구, 낙성대',
+	else: '기타',
+};

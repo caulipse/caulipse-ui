b05c03b7b9209bfe744cfb70c43bde0013e5be9d
Jeyoung-Park <jason9380@naver.com>
2022-04-22 20:54:33 +0900

    fix: studyDetail에 mapper 적용

--- src/app/studyDetail/studyInfo/StudyInfoPresenter.tsx
+++ src/app/studyDetail/studyInfo/StudyInfoPresenter.tsx
@@ -1,12 +1,14 @@
+import { frequencyEnum, locationEnum, weekdayEnum } from '@src/api/types';
 import { getMainCategoryLabel, getSubCategoryLabel } from '@src/app/shared/utils/category';
+import { frequencyMapper, locationMapper, weekdayMapper } from '@src/app/shared/utils/studyMapper';
 import React from 'react';
 import './styles.scss';
 
 interface StudyInfoPresenterProps {
 	categoryCode: string;
-	weekday: string;
-	frequency: string;
-	location: string;
+	weekday: weekdayEnum;
+	frequency: frequencyEnum;
+	location: locationEnum;
 }
 const StudyInfoPresenter = ({ categoryCode, weekday, frequency, location }: StudyInfoPresenterProps): JSX.Element => {
 	return (
@@ -22,15 +24,15 @@ const StudyInfoPresenter = ({ categoryCode, weekday, frequency, location }: Stud
 			<div className="study-sub-info-container">
 				<div className="mr16">
 					<div className="study-sub-info-title">요일</div>
-					<div className="study-sub-info-content">{weekday}</div>
+					<div className="study-sub-info-content">{weekdayMapper[weekday]}</div>
 				</div>
 				<div className="mr16">
 					<div className="study-sub-info-title">빈도</div>
-					<div className="study-sub-info-content">{frequency}</div>
+					<div className="study-sub-info-content">{frequencyMapper[frequency]}</div>
 				</div>
 				<div>
 					<div className="study-sub-info-title">장소</div>
-					<div className="study-sub-info-content">{location}</div>
+					<div className="study-sub-info-content">{locationMapper[location]}</div>
 				</div>
 			</div>
 		</div>

b5fbe526933afce6a56e4a91d265a257a383f310
Jeyoung-Park <jason9380@naver.com>
2022-04-28 00:19:34 +0900

    feat: isAfterEmailVerification로 워딩 변경

--- src/pages/signup/resetPwPage/index.tsx
+++ src/pages/signup/resetPwPage/index.tsx
@@ -3,9 +3,10 @@ import { validateEmail } from '@src/app/shared/utils/validation';
 import CommonButton from '@src/components/common/button/CommonButton';
 import { ButtonTypeEnum } from '@src/components/common/button/types';
 import CommonTextField from '@src/components/common/textfield/CommonTextField';
-import React, { KeyboardEvent, useCallback, useEffect, useState } from 'react';
+import React, { KeyboardEvent, useCallback, useEffect, useMemo, useState } from 'react';
 import { IoArrowBack } from 'react-icons/io5';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import QueryString from 'qs';
 import logoDefaultWhite from '@src/assets/img/logo/logoDefaultWhite.svg';
 import './index.scss';
 import usePatchResetPwMail from '@src/hooks/remotes/user/usePatchResetPwMail';
@@ -13,9 +14,15 @@ import usePatchResetPwMail from '@src/hooks/remotes/user/usePatchResetPwMail';
 const ResetPwPage = (): JSX.Element => {
 	const history = useHistory();
 	const patchResetPwMail = usePatchResetPwMail();
-
+	const location = useLocation();
+	const id = QueryString.parse(location?.search, { ignoreQueryPrefix: true })?.id;
+	const paramEmail = QueryString.parse(location?.search, { ignoreQueryPrefix: true })?.email;
 	const [email, setEmail] = useState<string>('');
 	const [emailHelperText, setEmailHelperText] = useState<string>('');
+
+	const isAfterEmailVerification = useMemo(() => {
+		return id && paramEmail;
+	}, [id, paramEmail]);
 
 	const handleNavigatePrivacyPolicy = () => {
 		// TODO: 개인정보처리방침 이동
@@ -61,8 +68,12 @@ const ResetPwPage = (): JSX.Element => {
 			</Box>
 			<Box className="reset-pw-body-align-self">
 				<Box className="reset-pw-body-con">
-					<Box className="reset-pw-body-title">암호 재설정</Box>
-					<Box className="reset-pw-body-subtitle">이메일 인증을 통해 암호를 재설정 할 수 있어요.</Box>
+					<Box className="reset-pw-body-title">{isAfterEmailVerification ? '암호 변경' : '암호 재설정'}</Box>
+					<Box className="reset-pw-body-subtitle">
+						{isAfterEmailVerification
+							? '양식에 맞춰 새 암호를 입력해주세요.'
+							: '이메일 인증을 통해 암호를 재설정 할 수 있어요.'}
+					</Box>
 					<CommonTextField
 						className="reset-pw-body-input"
 						value={email}

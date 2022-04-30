bf092982abab41a0b2287aa3962ab7979a10a6a6
Jeyoung-Park <jason9380@naver.com>
2022-05-01 04:10:38 +0900

    fix: logout시 snackbar

--- src/hooks/remotes/user/usePatchLogout.ts
+++ src/hooks/remotes/user/usePatchLogout.ts
@@ -1,9 +1,11 @@
 import API from '@src/api';
+import useSnackbar from '@src/hooks/snackbar/useSnackbar';
 import { useMutation, useQuery } from 'react-query';
 import { useHistory } from 'react-router-dom';
 
 export default () => {
 	const history = useHistory();
+	const { openSnackbar } = useSnackbar();
 
 	const mutation = async (): Promise<any> => {
 		const res = await API.logout();
@@ -12,7 +14,7 @@ export default () => {
 	return useMutation(mutation, {
 		onSuccess: (response: any) => {
 			console.log(response);
-			history.push('/');
+			openSnackbar('로그아웃되었습니다.');
 		},
 		onError: (e: Error) => {
 			console.error(e.message);

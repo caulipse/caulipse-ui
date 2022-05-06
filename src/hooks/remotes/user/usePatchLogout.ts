1c0ccd2f373a30773495f0078f9f8bc7f72a8e44
Jeyoung-Park <jason9380@naver.com>
2022-05-06 18:49:00 +0900

    fix: 로그아웃시 localStorage에서 userId 값 삭제

--- src/hooks/remotes/user/usePatchLogout.ts
+++ src/hooks/remotes/user/usePatchLogout.ts
@@ -1,11 +1,14 @@
 import API from '@src/api';
 import useSnackbar from '@src/hooks/snackbar/useSnackbar';
+import { useAtom } from 'jotai';
 import { useMutation, useQuery } from 'react-query';
 import { useHistory } from 'react-router-dom';
+import { userState as globalUserState } from '@src/state';
 
 export default () => {
 	const history = useHistory();
 	const { openSnackbar } = useSnackbar();
+	const [userState, setUserState] = useAtom(globalUserState);
 
 	const mutation = async (): Promise<any> => {
 		const res = await API.logout();
@@ -15,6 +18,7 @@ export default () => {
 		onSuccess: (response: any) => {
 			console.log(response);
 			openSnackbar('로그아웃되었습니다.');
+			setUserState({ ...userState, userId: '' });
 		},
 		onError: (e: Error) => {
 			console.error(e.message);

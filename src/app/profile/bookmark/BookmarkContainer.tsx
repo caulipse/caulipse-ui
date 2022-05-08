cd33ce9eb80753a84b23d0d57f752c16a9af883c
Jeyoung-Park <jason9380@naver.com>
2022-05-08 18:02:11 +0900

    fix: 북마크 서버  리스폰스 변경에 따른 대응

--- src/app/profile/bookmark/BookmarkContainer.tsx
+++ src/app/profile/bookmark/BookmarkContainer.tsx
@@ -4,8 +4,7 @@ import Loader from '@src/components/common/loader/Loader';
 import BookmarkPresenter from './BookmarkPresenter';
 
 const BookmarkContainer = (): JSX.Element => {
-	const { isLoading, data } = useFetchBookmarks();
-	const bookmarks = data?.bookmarks;
+	const { isLoading, data: bookmarks } = useFetchBookmarks();
 
 	const openedBookmarks = bookmarks?.filter((item) => new Date(item.dueDate) >= new Date());
 	const closedBookmarks = bookmarks?.filter((item) => new Date(item.dueDate) < new Date());

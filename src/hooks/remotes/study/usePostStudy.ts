4c45c899d027e1d9c79034a277f6343323e1a63b
Jeyoung-Park <jason9380@naver.com>
2022-05-16 00:40:17 +0900

    fix: 스터디 모집 이후 스낵바 호출

--- src/hooks/remotes/study/usePostStudy.ts
+++ src/hooks/remotes/study/usePostStudy.ts
@@ -1,10 +1,8 @@
 import { useMutation, useQueryClient } from 'react-query';
 import API from '@src/api';
 import { IRequestPostStudy } from '@src/api/request/study';
-import useSnackbar from '@src/hooks/snackbar/useSnackbar';
 
-export default (callback: () => void) => {
-	const { openSnackbar } = useSnackbar();
+export default () => {
 	const mutation = async (request: IRequestPostStudy) => {
 		const res = await API.postStudy(request);
 		return res.data;
@@ -13,8 +11,6 @@ export default (callback: () => void) => {
 	return useMutation(mutation, {
 		onSuccess: (response: any) => {
 			console.log(response);
-			callback();
-			openSnackbar('스터디 모집글 등록 완료');
 		},
 		onError: (e: Error) => {
 			console.error(e.message);

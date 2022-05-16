3b0d8bde298259620aca2ec99e75d05954cbae98
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 <mirijo02233092@gmail.com>
2022-05-16 21:39:28 +0900

    fix: 참가 신청 및 참가 현황 API 엔드포인트 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -92,7 +92,7 @@ const API = {
 	},
 	// 현재 참가 중인 사용자 목록을 읽어옵니다.
 	getStudyParticipants(id: string) {
-		return client.get(`/study/user/${id}/participants`);
+		return client.get(`/study/${id}/user/participants`);
 	},
 	// 현재 참가 신청 중인 사용자 목록을 읽어옵니다.
 	getStudyUsers(id: string) {
@@ -100,7 +100,7 @@ const API = {
 	},
 	// 스터디 참가 신청
 	postStudyUser(request: IRequestPostStudyUser) {
-		return client.post(`/study/user/${request.id}`, request.data);
+		return client.post(`/study/${request.id}/user`, request.data);
 	},
 	// 참가신청 현황 수정
 	patchStudyUser(request: IRequestPatchStudyUser) {

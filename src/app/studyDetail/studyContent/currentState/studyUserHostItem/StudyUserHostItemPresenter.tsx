284f155012378df905a8832993dbdd37a7d0ed6b
Jeyoung-Park <jason9380@naver.com>
2022-05-17 22:59:51 +0900

    fix: 참가현황에 프로필 사진 설정

--- src/app/studyDetail/studyContent/currentState/studyUserHostItem/StudyUserHostItemPresenter.tsx
+++ src/app/studyDetail/studyContent/currentState/studyUserHostItem/StudyUserHostItemPresenter.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { StudyUser, UserProfile } from '@api/types';
 import useModal from '@src/hooks/modal/useModal';
 import './styles.scss';
+import { getProfileImgs } from '@src/app/shared/utils/profileImg';
+import defaultImg from '@src/assets/img/profileImg/default.svg';
 
 interface StudyUserHostItemPresenterProps {
 	user: UserProfile;
@@ -18,7 +20,17 @@ const StudyUserHostItemPresenter = ({ user }: StudyUserHostItemPresenterProps): 
 		<div className="study-user-host-item-container">
 			{user ? (
 				<>
-					<img className="study-user-host-item-img" width={40} height={40} alt="" />
+					<img
+						className="study-user-host-item-img"
+						src={
+							getProfileImgs().includes(user.image)
+								? require(`@src/assets/img/profileImg/${user.image}`).default
+								: defaultImg
+						}
+						width={40}
+						height={40}
+						alt=""
+					/>
 					<div className="study-user-host-item-column-container">
 						<div className="study-user-host-item-row-container">
 							<div className="study-user-host-item-username">{user.userName}</div>

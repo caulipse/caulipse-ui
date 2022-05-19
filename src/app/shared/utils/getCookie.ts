a6ad1eaa3f1575be7a7a607b6689c7be6f0958eb
Jeyoung-Park <jason9380@naver.com>
2022-05-20 00:12:26 +0900

    fix: getCookie 로직 수정

--- src/app/shared/utils/getCookie.ts
+++ src/app/shared/utils/getCookie.ts
@@ -1,18 +1,9 @@
-const getCookie = (params: string) => {
-	const key = `${params}=`;
-	const { cookie } = document;
-	let start = cookie.indexOf(key);
-	let value = '';
-	if (start !== -1) {
-		start += key.length;
-		let end = cookie.indexOf(';', start);
-
-		if (end !== -1) {
-			end = cookie.length;
-			value = cookie.substring(start, end);
-		}
-	}
-	return value;
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line consistent-return
+const getCookie = (name: string) => {
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	if (parts.length === 2) return parts.pop()?.split(';').shift();
 };
 
 export default getCookie;

9fb460457780edb29c75caef2ae4ecf40e7fe7f3
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 <mirijo02233092@gmail.com>
2022-06-04 22:29:29 +0900

    fix: issue/420

--- src/app/modal/StudyFilterModal.tsx
+++ src/app/modal/StudyFilterModal.tsx
@@ -64,68 +64,70 @@ const StudyFilterModal = ({ open, onClose }: IModalContainerCommonProps): JSX.El
 							초기화
 						</span>
 					</Container>
-					<Container className="study-filter-modal-row">
-						<span>스터디 빈도</span>
-						<Grid container spacing={1}>
-							{frequencies.map((item) => {
-								const handleClick = () => {
-									onChange('frequency', item);
-								};
-								return (
-									<Grid key={item} item xs={4} className="modal-chip-item">
+					<Container style={{ padding: '4rem 0 0 0', marginBottom: '1rem' }}>
+						<Container className="study-filter-modal-row">
+							<span>스터디 빈도</span>
+							<Grid container spacing={1}>
+								{frequencies.map((item) => {
+									const handleClick = () => {
+										onChange('frequency', item);
+									};
+									return (
+										<Grid key={item} item xs={4} className="modal-chip-item">
+											<Chip
+												label={frequencyMapper[item]}
+												selected={filter?.frequency?.includes(item)}
+												onClick={handleClick}
+											/>
+										</Grid>
+									);
+								})}
+							</Grid>
+						</Container>
+						<Container className="study-filter-modal-row">
+							<span>요일</span>
+							<Container className="study-filter-modal-row-flex-container study-filter-modal-row-flex-container-days">
+								{days.map((item) => {
+									const handleClick = () => {
+										onChange('weekday', item);
+									};
+									return (
 										<Chip
-											label={frequencyMapper[item]}
-											selected={filter?.frequency?.includes(item)}
+											key={item}
+											label={weekdayMapper[item]}
+											selected={filter?.weekday?.includes(item)}
 											onClick={handleClick}
 										/>
-									</Grid>
-								);
-							})}
-						</Grid>
-					</Container>
-					<Container className="study-filter-modal-row">
-						<span>요일</span>
-						<Container className="study-filter-modal-row-flex-container study-filter-modal-row-flex-container-days">
-							{days.map((item) => {
-								const handleClick = () => {
-									onChange('weekday', item);
-								};
-								return (
-									<Chip
-										key={item}
-										label={weekdayMapper[item]}
-										selected={filter?.weekday?.includes(item)}
-										onClick={handleClick}
-									/>
-								);
-							})}
+									);
+								})}
+							</Container>
+						</Container>
+						<Container className="study-filter-modal-row">
+							<span>장소</span>
+							<span className="study-filter-modal-sub-text">최대 3개까지 선택 가능합니다</span>
+							<Container className="study-filter-modal-row-flex-container study-filter-modal-row-flex-container-places">
+								{places.map((item) => {
+									const handleClick = () => {
+										onChange('location', item);
+									};
+									return (
+										<Chip
+											key={item}
+											label={locationMapper[item]}
+											selected={filter?.location?.includes(item)}
+											onClick={handleClick}
+										/>
+									);
+								})}
+							</Container>
+						</Container>
+						<Container className="study-filter-modal-hide-row">
+							<span className="study-filter-modal-hide-row-title">마감항목 숨기기</span>
+							<Switch onChange={onChangeIsHide} checked={isHide} />
 						</Container>
 					</Container>
-					<Container className="study-filter-modal-row">
-						<span>장소</span>
-						<span className="study-filter-modal-sub-text">최대 3개까지 선택 가능합니다</span>
-						<Container className="study-filter-modal-row-flex-container study-filter-modal-row-flex-container-places">
-							{places.map((item) => {
-								const handleClick = () => {
-									onChange('location', item);
-								};
-								return (
-									<Chip
-										key={item}
-										label={locationMapper[item]}
-										selected={filter?.location?.includes(item)}
-										onClick={handleClick}
-									/>
-								);
-							})}
-						</Container>
-					</Container>
-					<Container className="study-filter-modal-hide-row">
-						<span className="study-filter-modal-hide-row-title">마감항목 숨기기</span>
-						<Switch onChange={onChangeIsHide} checked={isHide} />
-					</Container>
+					<CommonButton type={ButtonTypeEnum.secondary} title="적용" onClick={onClick} />
 				</Container>
-				<CommonButton type={ButtonTypeEnum.secondary} title="적용" onClick={onClick} />
 			</Container>
 		</Modal>
 	);

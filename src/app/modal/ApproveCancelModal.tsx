958cabf0afd40b0049d07ff828e967259f0897d9
Jeyoung-Park <jason9380@naver.com>
2022-06-05 15:41:26 +0900

    fix: 호출하는 api 수정

--- src/app/modal/ApproveCancelModal.tsx
+++ src/app/modal/ApproveCancelModal.tsx
@@ -8,22 +8,41 @@ import { SnackbarTypeEnum } from '@common/snackbar/types';
 import usePatchStudyUserAccept from '@src/hooks/remotes/studyUser/usePatchStudyUserAccept';
 import { useAtom } from 'jotai';
 import globalState, { modalState } from '@src/state';
+import { useQueryClient } from 'react-query';
+import useDeleteStudyUser from '@src/hooks/remotes/studyUser/useDeleteStudyUser';
+import QUERY_KEY from '@src/hooks/remotes';
 
 const ApproveCancelModal = ({ open, onClose }: IModalContainerCommonProps): JSX.Element => {
 	const { openSnackbar } = useSnackbar();
 	const [modal] = useAtom(modalState);
 
-	const patchStudyUserAccept = usePatchStudyUserAccept();
+	const client = useQueryClient();
+
+	const deleteStudyUser = useDeleteStudyUser();
 	const onClick = () => {
-		patchStudyUserAccept.mutate({
-			id: modal.params.studyId,
-			data: {
-				accept: false,
-				userId: modal.params.userId,
-			},
-		});
-		onClose(false);
-		openSnackbar('스터디 수락을 취소하였습니다', SnackbarTypeEnum.secondary);
+		deleteStudyUser.mutate(
+			{ studyId: modal.params.studyId, userId: modal.params.userId },
+			{
+				onSuccess: () => {
+					onClose(false);
+					openSnackbar('스터디 수락 취소하였습니다.', SnackbarTypeEnum.secondary);
+					client.refetchQueries(`${QUERY_KEY.FETCH_STUDY}/${modal.params.studyId}`);
+				},
+				onError: () => {
+					onClose(false);
+					openSnackbar('스터디 신청 취소에 실패하였습니다.', SnackbarTypeEnum.secondary);
+				},
+			}
+		);
+		// patchStudyUserAccept.mutate({
+		// 	id: modal.params.studyId,
+		// 	data: {
+		// 		accept: false,
+		// 		userId: modal.params.userId,
+		// 	},
+		// });
+		// onClose(false);
+		// openSnackbar('스터디 수락을 취소하였습니다', SnackbarTypeEnum.secondary);
 	};
 
 	const nickname = modal.params.username;

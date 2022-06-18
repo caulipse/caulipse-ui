3cda07532f8eb49f7dc49d34c91fb8ae16789119
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 <mirijo02233092@gmail.com>
2022-06-18 23:11:45 +0900

    issue/502

--- src/app/shared/components/card/StudyCardPresenter.tsx
+++ src/app/shared/components/card/StudyCardPresenter.tsx
@@ -4,6 +4,8 @@ import { Study } from '@api/types';
 import ProgressBar from '@common/progress/ProgressBar';
 import { getMainCategoryLabel, getSubCategoryLabel } from '@shared/utils/category';
 import useWindowDimensions from '@src/hooks/useWindowDimensions';
+import { differenceInDays } from 'date-fns';
+import classNames from 'classnames';
 
 import './index.scss';
 import { getDday } from '../../utils/date';
@@ -18,14 +20,26 @@ const StudyCardPresenter = ({ study }: PresenterProps): JSX.Element => {
 		return width > 1024;
 	}, [width]);
 
+	const isOpen = useMemo(() => {
+		const diff = differenceInDays(new Date(), new Date(study.dueDate));
+		return study?.isOpen && diff < 0;
+	}, [study]);
 	return (
 		<Container className="study-card-container">
 			<Container className="study-card-category-container">
-				{isDesktop && <Container className="study-card-date">{getDday(study.dueDate)}</Container>}
+				{isDesktop && (
+					<Container className={classNames('study-card-date', { 'study-card-date-disabled': !isOpen })}>
+						{isOpen ? getDday(study.dueDate) : '마감됨'}
+					</Container>
+				)}
 				<span className="study-card-category">
 					{getMainCategoryLabel(Number(study.categoryCode))} {'>'} {getSubCategoryLabel(Number(study.categoryCode))}
 				</span>
-				{!isDesktop && <Container className="study-card-date">{getDday(study.dueDate)}</Container>}
+				{!isDesktop && (
+					<Container className={classNames('study-card-date', { 'study-card-date-disabled': !isOpen })}>
+						{isOpen ? getDday(study.dueDate) : '마감됨'}
+					</Container>
+				)}
 			</Container>
 			<Typography className="study-card-title">{study.title}</Typography>
 			<Typography className="study-card-tag">
